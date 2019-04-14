83f21f38a33d13c632a9f958dc26335e8d61371c
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-14 15:42:50 -0700

    Adjust imports

--- examples/quick-start/crates/app-main/src/lib.rs
+++ examples/quick-start/crates/app-main/src/lib.rs
@@ -1,5 +1,5 @@
-use electron_sys::{app, browser_window, BrowserWindow};
-use wasm_bindgen::prelude::*;
+use electron_sys::{BrowserWindow, app, browser_window};
+use wasm_bindgen::{JsCast, prelude::*};
 
 fn create_window() -> Result<(), JsValue> {
     let options = browser_window::Options {
@@ -13,7 +13,6 @@ fn create_window() -> Result<(), JsValue> {
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    use wasm_bindgen::JsCast;
     console_error_panic_hook::set_once();
     let clo = Closure::wrap(Box::new(|| {
         create_window().unwrap();
