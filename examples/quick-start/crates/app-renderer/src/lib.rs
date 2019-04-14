d474497216bdefed4669a6e8d39c76f540b450c7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-13 23:56:03 -0700

    Renaming

--- examples/quick-start/crates/app-renderer/src/lib.rs
+++ examples/quick-start/crates/app-renderer/src/lib.rs
@@ -5,8 +5,8 @@ pub fn main() -> Result<(), JsValue> {
     let window = web_sys::window().unwrap_throw();
     let document = window.document().unwrap_throw();
     let body = document.body().unwrap_throw();
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-    body.append_child(&val)?;
+    let elem = document.create_element("p")?;
+    elem.set_inner_html("Hello from Rust!");
+    body.append_child(&elem)?;
     Ok(())
 }
