3636165a39b700c7916022866ceb4acfe9682dea
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-14 14:30:10 -0700

    Use console error panic hook in renderer too

--- examples/quick-start/crates/app-renderer/src/lib.rs
+++ examples/quick-start/crates/app-renderer/src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
     let window = web_sys::window().unwrap_throw();
     let document = window.document().unwrap_throw();
     let body = document.body().unwrap_throw();
