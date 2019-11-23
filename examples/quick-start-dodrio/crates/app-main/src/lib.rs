d5b06523fea2c4581d5c91a050159e1d3918e2d1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-23 14:33:44 -0700

    Update examples; change title and show about panel

--- examples/quick-start-dodrio/crates/app-main/src/lib.rs
+++ examples/quick-start-dodrio/crates/app-main/src/lib.rs
@@ -2,22 +2,24 @@ use electron_sys::{app, BrowserWindow};
 use js_sys::{Object, Reflect};
 use wasm_bindgen::{prelude::*, JsCast};
 
-fn create_window() -> Result<(), JsValue> {
-    let window = BrowserWindow::new(Some(&{
+fn create_window() -> Result<BrowserWindow, JsValue> {
+    let win = BrowserWindow::new(Some(&{
         let res = Object::new();
-        Reflect::set(&res, &"height".into(), &800.into()).unwrap();
-        Reflect::set(&res, &"width".into(), &600.into()).unwrap();
+        Reflect::set(&res, &"width".into(), &640.into()).unwrap();
+        Reflect::set(&res, &"height".into(), &480.into()).unwrap();
         res
     }));
-    window.load_file("..\\..\\..\\..\\..\\index.html".into()); // FIXME
-    Ok(())
+    win.load_file("..\\..\\..\\..\\..\\index.html".into()); // FIXME
+    Ok(win)
 }
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
     let clo = Closure::wrap(Box::new(|| {
-        create_window().unwrap();
+        let win = create_window().unwrap();
+        win.set_title(&"Hello Electron from Rust! ⚛️🦀🕸🚀".into());
+        app.show_about_panel();
     }) as Box<dyn FnMut()>);
     app.on("ready".into(), clo.as_ref().unchecked_ref());
     clo.forget();
