4c7e70eeddde619b062a0b641909642bf1397cfd
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-23 18:35:20 -0700

    Bindings for SetAboutPanelOptions object

--- src/app.rs
+++ src/app.rs
@@ -299,6 +299,124 @@ impl RelaunchOptions {
     }
 }
 
+#[wasm_bindgen]
+pub struct SetAboutPanelOptions {
+    application_name: Option<JsString>,
+    application_version: Option<JsString>,
+    copyright: Option<JsString>,
+    version: Option<JsString>,   // FIXME: macos
+    credits: Option<JsString>,   // FIXME: macos
+    authors: JsString,           // FIXME: linux
+    website: Option<JsString>,   // FIXME: linux
+    icon_path: Option<JsString>, // FIXME: linux
+}
+
+#[wasm_bindgen]
+impl SetAboutPanelOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        application_name: Option<JsString>,
+        application_version: Option<JsString>,
+        copyright: Option<JsString>,
+        version: Option<JsString>,   // FIXME: macos
+        credits: Option<JsString>,   // FIXME: macos
+        authors: JsString,           // FIXME: linux
+        website: Option<JsString>,   // FIXME: linux
+        icon_path: Option<JsString>, // FIXME: linux
+    ) -> SetAboutPanelOptions {
+        SetAboutPanelOptions {
+            application_name,
+            application_version,
+            copyright,
+            version,
+            credits,
+            authors,
+            website,
+            icon_path,
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn application_name(&self) -> Option<JsString> {
+        self.application_name.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_application_name(&mut self, application_name: Option<JsString>) {
+        self.application_name = application_name;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn application_version(&self) -> Option<JsString> {
+        self.application_version.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_application_version(&mut self, application_version: Option<JsString>) {
+        self.application_version = application_version;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn copyright(&self) -> Option<JsString> {
+        self.copyright.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_copyright(&mut self, copyright: Option<JsString>) {
+        self.copyright = copyright;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn version(&self) -> Option<JsString> {
+        self.version.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_version(&mut self, version: Option<JsString>) {
+        self.version = version;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn credits(&self) -> Option<JsString> {
+        self.credits.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_credits(&mut self, credits: Option<JsString>) {
+        self.credits = credits;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn authors(&self) -> JsString {
+        self.authors.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_authors(&mut self, authors: JsString) {
+        self.authors = authors;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn website(&self) -> Option<JsString> {
+        self.website.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_website(&mut self, website: Option<JsString>) {
+        self.website = website;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon_path(&self) -> Option<JsString> {
+        self.icon_path.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon_path(&mut self, icon_path: Option<JsString>) {
+        self.icon_path = icon_path;
+    }
+}
+
 #[wasm_bindgen(module = "electron")]
 extern {
     /// The electron app interface type.
