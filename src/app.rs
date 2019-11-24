c129ec5077c4812cb100775b67ca8810cc0be300
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-23 18:34:24 -0700

    Bindings for GetLoginItemSettingsOptions object

--- src/app.rs
+++ src/app.rs
@@ -140,6 +140,40 @@ impl GetLoginItemSettings {
 }
 
 #[wasm_bindgen]
+pub struct GetLoginItemSettingsOptions {
+    path: Option<JsString>, // FIXME: windows
+    args: JsString,         // FIXME: windows
+}
+
+#[wasm_bindgen]
+impl GetLoginItemSettingsOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(path: Option<JsString>, args: JsString) -> GetLoginItemSettingsOptions {
+        GetLoginItemSettingsOptions { path, args }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn path(&self) -> Option<JsString> {
+        self.path.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_path(&mut self, path: Option<JsString>) {
+        self.path = path;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn args(&self) -> JsString {
+        self.args.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_args(&mut self, args: JsString) {
+        self.args = args;
+    }
+}
+
+#[wasm_bindgen]
 pub struct JumpListSettings {
     min_items: usize,
     removed_items: Array,
