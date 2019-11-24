5fb1d951ce4fa2f0d50a06e58dafa19f4bcfbc48
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-24 15:14:14 -0700

    Bindings for ShortcutDetails

--- src/object/shortcut_details.rs
+++ src/object/shortcut_details.rs
@@ -1,6 +1,108 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub type ShortcutDetails;
+pub struct ShortcutDetails {
+    app_user_model_id: Option<JsString>,
+    args: Option<JsString>,
+    cwd: Option<JsString>,
+    description: Option<JsString>,
+    icon: Option<JsString>,
+    icon_index: Option<usize>,
+    target: JsString,
+}
+
+#[wasm_bindgen]
+impl ShortcutDetails {
+    #[allow(clippy::too_many_arguments)]
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        app_user_model_id: Option<JsString>,
+        args: Option<JsString>,
+        cwd: Option<JsString>,
+        description: Option<JsString>,
+        icon: Option<JsString>,
+        icon_index: Option<usize>,
+        target: JsString,
+    ) -> ShortcutDetails {
+        ShortcutDetails {
+            app_user_model_id,
+            args,
+            cwd,
+            description,
+            icon,
+            icon_index,
+            target,
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn app_user_model_id(&self) -> Option<JsString> {
+        self.app_user_model_id.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_app_user_model_id(&mut self, app_user_model_id: Option<JsString>) {
+        self.app_user_model_id = app_user_model_id;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn args(&self) -> Option<JsString> {
+        self.args.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_args(&mut self, args: Option<JsString>) {
+        self.args = args;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn cwd(&self) -> Option<JsString> {
+        self.cwd.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_cwd(&mut self, cwd: Option<JsString>) {
+        self.cwd = cwd;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn description(&self) -> Option<JsString> {
+        self.description.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_description(&mut self, description: Option<JsString>) {
+        self.description = description;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon(&self) -> Option<JsString> {
+        self.icon.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon(&mut self, icon: Option<JsString>) {
+        self.icon = icon;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon_index(&self) -> Option<usize> {
+        self.icon_index
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon_index(&mut self, icon_index: Option<usize>) {
+        self.icon_index = icon_index;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn target(&self) -> JsString {
+        self.target.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_target(&mut self, target: JsString) {
+        self.target = target;
+    }
 }
