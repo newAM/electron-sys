f5f9f161cdac5ddbc2e24d96d9114c7c65c78679
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-23 18:34:56 -0700

    Bindings for MoveToApplicationsFolderOptions object

--- src/app.rs
+++ src/app.rs
@@ -241,6 +241,31 @@ impl JumpListSettings {
 }
 
 #[wasm_bindgen]
+pub struct MoveToApplicationsFolderOptions {
+    conflict_handler: Function,
+}
+
+#[wasm_bindgen]
+impl MoveToApplicationsFolderOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(conflict_handler: &Function) -> MoveToApplicationsFolderOptions {
+        MoveToApplicationsFolderOptions {
+            conflict_handler: conflict_handler.clone(),
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn conflict_handler(&self) -> Function {
+        self.conflict_handler.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_conflict_handler(&mut self, conflict_handler: &Function) {
+        self.conflict_handler = conflict_handler.clone();
+    }
+}
+
+#[wasm_bindgen]
 pub struct RelaunchOptions {
     args: JsString,
     exec_path: Option<JsString>,
