ec56bd86df9731403215e16b6aefb30495238e67
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-24 20:04:09 -0700

    Bindings for contextBridge module

--- src/module/context_bridge.rs
+++ src/module/context_bridge.rs
@@ -1,10 +1,15 @@
+use js_sys::{JsString, Object};
+use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
 extern {
-    #[wasm_bindgen]
+    #[wasm_bindgen(extends = EventEmitter)]
     pub type ContextBridge;
 
     #[wasm_bindgen(js_name = "contextBridge")]
     pub static context_bridge: ContextBridge;
+
+    #[wasm_bindgen(method, js_name = "exposeInMainWorld")]
+    pub fn expose_in_main_world(this: &ContextBridge, api_key: &JsString, api: &Object);
 }
