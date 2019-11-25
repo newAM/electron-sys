b3c7d2b189d2e7f29ae8d8ed815c68c731135907
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-24 20:14:29 -0700

    Bindings for Size

--- src/object/size.rs
+++ src/object/size.rs
@@ -1,6 +1,36 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub type Size;
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub struct Size {
+    height: usize,
+    width: usize
+}
+
+#[wasm_bindgen]
+impl Size {
+    #[wasm_bindgen(constructor)]
+    pub fn new(height: usize, width: usize) -> Size {
+        Size { height, width }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_height(&mut self, height: usize) {
+        self.height = height;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn width(&self) -> usize {
+        self.height
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_width(&mut self, width: usize) {
+        self.width = width;
+    }
 }
