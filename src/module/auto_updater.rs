1622d9c903db048666c815937de61eb4576ad681
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-24 20:00:26 -0700

    Bindings for autoUpdator module

--- src/module/auto_updater.rs
+++ src/module/auto_updater.rs
@@ -1,3 +1,4 @@
+use crate::object::FeedUrlOptions;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,22 @@ extern {
 
     #[wasm_bindgen(js_name = "autoUpdater")]
     pub static auto_updater: AutoUpdater;
+
+    /// Asks the server whether there is an update. You must call `set_feed_url` before using this
+    /// API.
+    #[wasm_bindgen(method, js_name = "checkForUpdates")]
+    pub fn check_for_updates(this: &AutoUpdater);
+
+    /// The current update feed URL.
+    #[wasm_bindgen(method, js_name = "getFeedURL")]
+    pub fn get_feed_url(this: &AutoUpdater);
+
+    /// Restarts the app and installs the update after it has been downloaded. It should only be
+    /// called after `update-downloaded` has been emitted.
+    #[wasm_bindgen(method, js_name = "quitAndInstall")]
+    pub fn quit_and_install(this: &AutoUpdater);
+
+    /// Sets the `url` and initialize the auto updater.
+    #[wasm_bindgen(method, js_name = "setFeedURL")]
+    pub fn set_feed_url(this: &AutoUpdater, options: FeedUrlOptions);
 }
