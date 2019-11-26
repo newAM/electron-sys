d32b47f3e7b6e7fbe2b0759b2dc7b509873b85a1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 21:45:30 -0700

    Partial bindings for clipboard module

--- src/module/clipboard.rs
+++ src/module/clipboard.rs
@@ -1,3 +1,6 @@
+use crate::object::ReadBookmark;
+use js_sys::{Array, JsString};
+use node_sys::Buffer;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -6,4 +9,22 @@ extern {
     pub type Clipboard;
 
     pub static clipboard: Clipboard;
+
+    #[wasm_bindgen(method, js_name = "availableFormats")]
+    pub fn available_formats(this: &Clipboard, r#type: Option<&JsString>) -> Array;
+
+    #[wasm_bindgen(method)]
+    pub fn clear(this: &Clipboard, r#type: Option<&JsString>);
+
+    #[wasm_bindgen(method)]
+    pub fn has(this: &Clipboard, format: &JsString, r#type: Option<&JsString>) -> bool;
+
+    #[wasm_bindgen(method)]
+    pub fn read(this: &Clipboard, format: &JsString) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "readBookmark")]
+    pub fn read_bookmark(this: &Clipboard) -> ReadBookmark;
+
+    #[wasm_bindgen(method, js_name = "readBuffer")]
+    pub fn read_buffer(this: &Clipboard) -> Buffer;
 }
