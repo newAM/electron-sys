c9ff6d885a1e153c3a1345c2672abe570aa1d3e7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 09:58:45 -0700

    Bindings for OpenDevToolsOptions

---
+++ src/object/open_dev_tools_options.rs
@@ -0,0 +1,26 @@
+use js_sys::JsString;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct OpenDevToolsOptions {
+    activate: Option<bool>,
+    mode: JsString,
+}
+
+#[wasm_bindgen]
+impl OpenDevToolsOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(activate: Option<bool>, mode: JsString) -> OpenDevToolsOptions {
+        OpenDevToolsOptions { activate, mode }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn activate(&self) -> Option<bool> {
+        self.activate
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn mode(&self) -> JsString {
+        self.mode.clone()
+    }
+}
