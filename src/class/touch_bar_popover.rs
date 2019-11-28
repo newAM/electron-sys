0b209fc2e23718c7753632d7d0fb3b04afec446a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 20:14:30 -0700

    Bindings for TouchBarPopover class

--- src/class/touch_bar_popover.rs
+++ src/class/touch_bar_popover.rs
@@ -1,3 +1,5 @@
+use crate::{class::NativeImage, interface::TouchBarPopoverOptions};
+use js_sys::JsString;
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +9,23 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-popover
     pub type TouchBarPopover;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarPopoverOptions) -> TouchBarPopover;
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter)]
+    pub fn icon(this: &TouchBarPopover) -> NativeImage;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_icon(this: &TouchBarPopover, value: NativeImage);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn label(this: &TouchBarPopover) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_label(this: &TouchBarPopover, value: JsString);
 }
