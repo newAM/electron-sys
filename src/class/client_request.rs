d88ffb16d1799ea8385486fac85c51356992f6a8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 18:16:55 -0700

    Bindings for ClientRequest class

--- src/class/client_request.rs
+++ src/class/client_request.rs
@@ -1,4 +1,6 @@
-use node_sys::events::EventEmitter;
+use crate::interface::{ClientRequestOptions, UploadProgress};
+use js_sys::{Function, JsString};
+use node_sys::{events::EventEmitter, Buffer};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +9,43 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/client-request
     pub type ClientRequest;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: ClientRequestOptions) -> ClientRequest;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method)]
+    pub fn abort(this: &ClientRequest);
+
+    #[wasm_bindgen(method)]
+    pub fn end(this: &ClientRequest, chunk: Option<&Buffer>, encoding: Option<&JsString>, callback: Option<&Function>);
+
+    #[wasm_bindgen(method)]
+    pub fn follow_redirect(this: &ClientRequest);
+
+    #[wasm_bindgen(method, js_name = "getHeader")]
+    pub fn get_header(this: &ClientRequest, name: &JsString) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "getUploadProgress")]
+    pub fn get_upload_progress(this: &ClientRequest) -> UploadProgress;
+
+    #[wasm_bindgen(method, js_name = "removeHeader")]
+    pub fn remove_header(this: &ClientRequest, name: &JsString);
+
+    #[wasm_bindgen(method, js_name = "setHeader")]
+    pub fn set_header(this: &ClientRequest, name: &JsString, value: &JsString);
+
+    #[wasm_bindgen(method)]
+    pub fn write(this: &ClientRequest, chunk: &Buffer, encoding: Option<&JsString>, callback: Option<&Function>);
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter, js_name = "chunkedEncoding")]
+    pub fn chunked_encoding(this: &ClientRequest) -> bool;
+
+    #[wasm_bindgen(method, setter, js_name = "chunkedEncoding")]
+    pub fn set_chunked_encoding(this: &ClientRequest, value: bool);
 }
