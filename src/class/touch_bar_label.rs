2c11f0fa34fe9eca383d861e2f4c956dccc01877
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 20:24:00 -0700

    Bindings for TouchBarLabel  class

--- src/class/touch_bar_label.rs
+++ src/class/touch_bar_label.rs
@@ -1,3 +1,5 @@
+use crate::interface::TouchBarLabelOptions;
+use js_sys::JsString;
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +9,29 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-label
     pub type TouchBarLabel;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(this: TouchBarLabelOptions) -> TouchBarLabel;
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter, js_name = "accessibilityLabel")]
+    pub fn accessibility_label(this: &TouchBarLabel) -> JsString;
+
+    #[wasm_bindgen(method, setter, js_name = "accessibilityLabel")]
+    pub fn set_accessibility_label(this: &TouchBarLabel, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn label(this: &TouchBarLabel) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_label(this: &TouchBarLabel, value: JsString);
+
+    #[wasm_bindgen(method, getter, js_name = "textColor")]
+    pub fn text_color(this: &TouchBarLabel) -> JsString;
+
+    #[wasm_bindgen(method, getter, js_name = "textColor")]
+    pub fn set_text_color(this: &TouchBarLabel, value: JsString);
 }
