e676418d57ea6d9014e10a2fa1f686e11c6a83cb
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 18:57:38 -0700

    Bindings for TouchBarSlider class

--- src/class/touch_bar_slider.rs
+++ src/class/touch_bar_slider.rs
@@ -1,3 +1,5 @@
+use crate::interface::TouchBarSliderOptions;
+use js_sys::{JsString, Number};
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +9,36 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-slider
     pub type TouchBarSlider;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarSliderOptions) -> TouchBarSlider;
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter)]
+    pub fn label(this: &TouchBarSlider) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_label(this: &TouchBarSlider, value: &JsString);
+
+    #[wasm_bindgen(method, getter, js_name = "maxValue")]
+    pub fn max_value(this: &TouchBarSlider) -> Number;
+
+    #[wasm_bindgen(method, setter, js_name = "maxValue")]
+    pub fn set_max_value(this: &TouchBarSlider, value: &Number);
+
+    #[wasm_bindgen(method, getter, js_name = "minValue")]
+    pub fn min_value(this: &TouchBarSlider) -> Number;
+
+    #[wasm_bindgen(method, setter, js_name = "minValue")]
+    pub fn set_min_value(this: &TouchBarSlider, value: &Number);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn value(this: &TouchBarSlider) -> Number;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_value(this: &TouchBarSlider, value: &Number);
+
 }
