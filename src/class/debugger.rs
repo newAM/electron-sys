693840abc6dc0ea181493cc1dfa5dabc3e8e761c
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 18:34:11 -0700

    Bindings for Debugger class

--- src/class/debugger.rs
+++ src/class/debugger.rs
@@ -1,3 +1,4 @@
+use js_sys::{JsString, Promise};
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +8,18 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/debugger
     pub type Debugger;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method)]
+    pub fn attach(this: &Debugger, protocol_version: Option<JsString>);
+
+    #[wasm_bindgen(method)]
+    pub fn detach(this: &Debugger);
+
+    #[wasm_bindgen(method, js_name = "isAttached")]
+    pub fn is_attached(this: &Debugger);
+
+    #[wasm_bindgen(method, js_name = "sendCommand")]
+    pub fn send_command(this: &Debugger, method: &JsString, command_params: &JsValue) -> Promise;
 }
