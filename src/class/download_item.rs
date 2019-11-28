2e78b051546c620e9dcc1dd778337f670200876d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 17:12:08 -0700

    Stub for DownloadItem class

---
+++ src/class/download_item.rs
@@ -0,0 +1,10 @@
+use node_sys::events::EventEmitter;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "electron")]
+extern {
+    #[wasm_bindgen(extends = EventEmitter)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    /// Docs: http://electronjs.org/docs/api/download-item
+    pub type DownloadItem;
+}
