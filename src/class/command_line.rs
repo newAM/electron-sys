a5c213378ea44b1f7359a4c287c20f7ed1eb8114
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 18:21:43 -0700

    Bindings for CommandLine class

--- src/class/command_line.rs
+++ src/class/command_line.rs
@@ -1,4 +1,4 @@
-use js_sys::Object;
+use js_sys::{JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +7,19 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/command-line
     pub type CommandLine;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method, js_name = "appendArgument")]
+    pub fn append_argument(this: &CommandLine, value: &JsString);
+
+    #[wasm_bindgen(method, js_name = "appendSwitch")]
+    pub fn append_switch(this: &CommandLine, switch: &JsString, value: Option<&JsString>);
+
+    // FIXME: null is empty string from electron; we could return Option though
+    #[wasm_bindgen(method, js_name = "getSwitchValue")]
+    pub fn get_switch_value(this: &CommandLine, switch: &JsString) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "hasSwitch")]
+    pub fn has_switch(this: &CommandLine, switch: &JsString) -> bool;
 }
