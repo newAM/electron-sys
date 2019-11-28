ebd765cdb7ec1a5ecf154e9512a26d5afed4295e
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 19:56:07 -0700

    Bindings for TouchBarScrubber class

--- src/class/touch_bar_scrubber.rs
+++ src/class/touch_bar_scrubber.rs
@@ -1,3 +1,5 @@
+use crate::interface::TouchBarScrubberOptions;
+use js_sys::{Array, JsString};
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +9,47 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-scrubber
     pub type TouchBarScrubber;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarScrubberOptions) -> TouchBarScrubber;
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter)]
+    pub fn continuous(this: &TouchBarScrubber) -> bool;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_continuous(this: &TouchBarScrubber, value: bool);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn items(this: &TouchBarScrubber) -> Array;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_items(this: &TouchBarScrubber, value: &Array);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn mode(this: &TouchBarScrubber) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_mode(this: &TouchBarScrubber, value: &JsString);
+
+    #[wasm_bindgen(method, getter, js_name = "overlayStyle")]
+    pub fn overlay_style(this: &TouchBarScrubber) -> JsString;
+
+    #[wasm_bindgen(method, setter, js_name = "overlayStyle")]
+    pub fn set_overlay_style(this: &TouchBarScrubber, value: &JsString);
+
+    #[wasm_bindgen(method, getter, js_name = "selectedStyle")]
+    pub fn selected_style(this: &TouchBarScrubber) -> JsString;
+
+    #[wasm_bindgen(method, setter, js_name = "selectedStyle")]
+    pub fn set_selected_style(this: &TouchBarScrubber, value: &JsString);
+
+    #[wasm_bindgen(method, getter, js_name = "showArrowButtons")]
+    pub fn show_arrow_buttons(this: &TouchBarScrubber) -> bool;
+
+    #[wasm_bindgen(method, setter, js_name = "showArrowButtons")]
+    pub fn set_show_arrow_buttons(this: &TouchBarScrubber, value: bool);
 }
