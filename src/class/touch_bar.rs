cb2018ed9bdf7e7140e8826ab79a73a951a47888
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 20:45:05 -0700

    Bindings for TouchBar class

--- src/class/touch_bar.rs
+++ src/class/touch_bar.rs
@@ -1,3 +1,4 @@
+use crate::interface::TouchBarOptions;
 use js_sys::Object;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +8,17 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar
     pub type TouchBar;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarOptions) -> TouchBar;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method, getter, js_name = "escapeItem")]
+    pub fn escape_item(this: &TouchBar) -> Option<Object>;
+
+    #[wasm_bindgen(method, setter, js_name = "escapeItem")]
+    pub fn set_escape_item(this: &TouchBar, value: Option<Object>);
 }
