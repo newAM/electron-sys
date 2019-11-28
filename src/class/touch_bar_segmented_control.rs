84c08f2dd28f082f145643616602323d7dc2a43f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 19:47:08 -0700

    Bindings for TouchBarSegmentedControl class

--- src/class/touch_bar_segmented_control.rs
+++ src/class/touch_bar_segmented_control.rs
@@ -1,3 +1,5 @@
+use crate::interface::TouchBarSegmentedControlOptions;
+use js_sys::{Array, JsString};
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +9,29 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-segmented-control
     pub type TouchBarSegmentedControl;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarSegmentedControlOptions) -> TouchBarSegmentedControl;
+
+    // Instance Properties
+
+    #[wasm_bindgen(getter)]
+    pub fn segments(this: &TouchBarSegmentedControl) -> Array;
+
+    #[wasm_bindgen(getter)]
+    pub fn set_segments(this: &TouchBarSegmentedControl, value: &Array);
+
+    #[wasm_bindgen(getter, js_name = "segment_style")]
+    pub fn segment_style(this: &TouchBarSegmentedControl) -> JsString;
+
+    #[wasm_bindgen(setter, js_name = "segment_style")]
+    pub fn set_segment_style(this: &TouchBarSegmentedControl, value: JsString);
+
+    #[wasm_bindgen(getter, js_name = "selected_index")]
+    pub fn selected_index(this: &TouchBarSegmentedControl) -> usize;
+
+    #[wasm_bindgen(setter, js_name = "selected_index")]
+    pub fn set_selected_index(this: &TouchBarSegmentedControl, value: usize);
 }
