f1f077070b33c8e782c5a60a96743cdb54b3a480
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 18:05:01 -0700

    Bindings for BrowserWindowProxy class

--- src/class/browser_window_proxy.rs
+++ src/class/browser_window_proxy.rs
@@ -1,4 +1,4 @@
-use js_sys::Object;
+use js_sys::{JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +7,29 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/browser-window-proxy
     pub type BrowserWindowProxy;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method)]
+    pub fn blur(this: &BrowserWindowProxy);
+
+    #[wasm_bindgen(method)]
+    pub fn close(this: &BrowserWindowProxy);
+
+    #[wasm_bindgen(method)]
+    pub fn eval(this: &BrowserWindowProxy, code: &JsString);
+
+    #[wasm_bindgen(method)]
+    pub fn focus(this: &BrowserWindowProxy);
+
+    #[wasm_bindgen(method, js_name = "postMessage")]
+    pub fn post_message(this: &BrowserWindowProxy, message: &JsValue, target_origin: &JsString);
+
+    #[wasm_bindgen(method)]
+    pub fn print(this: &BrowserWindowProxy);
+
+    // Instance Properties
+
+    #[wasm_bindgen(method, getter)]
+    pub fn closed(this: &BrowserWindowProxy) -> bool;
 }
