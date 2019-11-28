d52f34f34db88cbb2a0a4004830d3bad14f751ce
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 20:27:58 -0700

    Bindings for TouchBarGroup class

--- src/class/touch_bar_group.rs
+++ src/class/touch_bar_group.rs
@@ -1,3 +1,4 @@
+use crate::interface::TouchBarGroupOptions;
 use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
@@ -7,4 +8,9 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/touch-bar-group
     pub type TouchBarGroup;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: TouchBarGroupOptions) -> TouchBarGroup;
 }
