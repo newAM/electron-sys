9efd595dcb1803fe69be5b0ba20eba4514785bd2
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-28 22:16:18 -0700

    Bindings for Menu class

--- src/class/menu.rs
+++ src/class/menu.rs
@@ -1,4 +1,8 @@
-use js_sys::Object;
+use crate::{
+    class::{BrowserWindow, MenuItem},
+    interface::PopupOptions,
+};
+use js_sys::{Array, JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +11,45 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/menu
     pub type Menu;
+
+    // Constructor
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Menu;
+
+    // Static Methods
+
+    #[wasm_bindgen(static_method_of = Menu, js_name = "buildFromTemplate")]
+    pub fn build_from_template(template: &Array) -> Menu;
+
+    #[wasm_bindgen(static_method_of = Menu, js_name = "getApplicationMenu")]
+    pub fn get_application_menu(template: &Array) -> Option<Menu>;
+
+    #[wasm_bindgen(static_method_of = Menu, js_name = "sendApplicationToFirstResponder")]
+    pub fn send_application_to_first_responder(action: &JsString);
+
+    #[wasm_bindgen(static_method_of = Menu, js_name = "setApplicationMenu")]
+    pub fn set_application_menu(menu: Option<Menu>);
+
+    // Instance Methods
+
+    #[wasm_bindgen(method)]
+    pub fn append(this: &Menu, menu_item: &MenuItem);
+
+    #[wasm_bindgen(method, js_name = "closePopup")]
+    pub fn close_popup(this: &Menu, browser_window: Option<BrowserWindow>);
+
+    #[wasm_bindgen(method, js_name = "getMenuItemById")]
+    pub fn get_menu_item_by_id(this: &Menu, id: &JsString) -> MenuItem;
+
+    #[wasm_bindgen(method)]
+    pub fn insert(this: &Menu, pos: usize, menu_item: &MenuItem);
+
+    #[wasm_bindgen(method)]
+    pub fn popup(this: &Menu, options: Option<PopupOptions>);
+
+    // Instance Properties
+
+    #[wasm_bindgen(method)]
+    pub fn items(this: &Menu) -> Array;
 }
