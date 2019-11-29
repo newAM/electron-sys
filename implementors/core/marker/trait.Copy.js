cded25c16720d824a7699796761445ebbd39600b
darinmorrison <darinmorrison@users.noreply.github.com>
2019-11-29 07:25:18 +0000

    Automated deployment: Fri Nov 29 07:25:18 UTC 2019 2695720e38c44c4883e7c62e2da99084a4178e01

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["electron_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"electron_sys/struct.Size.html\" title=\"struct electron_sys::Size\">Size</a>",synthetic:false,types:["electron_sys::interface::size::Size"]},];
+implementors["electron_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"electron_sys/struct.GetLoginItemSettings.html\" title=\"struct electron_sys::GetLoginItemSettings\">GetLoginItemSettings</a>",synthetic:false,types:["electron_sys::interface::get_login_item_settings::GetLoginItemSettings"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"electron_sys/struct.Size.html\" title=\"struct electron_sys::Size\">Size</a>",synthetic:false,types:["electron_sys::interface::size::Size"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
