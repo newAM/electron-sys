925f4aa1c15a2fd4c493f601ae867ba2a8768ec4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-28 19:22:11 -0700

    Bindings for Dock class

--- src/class/dock.rs
+++ src/class/dock.rs
@@ -1,4 +1,5 @@
-use js_sys::Object;
+use crate::class::{Menu, NativeImage};
+use js_sys::{JsString, Object, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,39 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/dock
     pub type Dock;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method)]
+    pub fn bounce(this: &Dock, kind: &JsString) -> usize;
+
+    #[wasm_bindgen(method, js_name = "cancelBounce")]
+    pub fn cancel_bounce(this: &Dock, id: usize);
+
+    #[wasm_bindgen(method, js_name = "downloadFinished")]
+    pub fn download_finished(this: &Dock, file_path: &JsString);
+
+    #[wasm_bindgen(method, js_name = "getBadge")]
+    pub fn get_badge(this: &Dock) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "getMenu")]
+    pub fn get_menu(this: &Dock) -> Option<Menu>;
+
+    #[wasm_bindgen(method)]
+    pub fn hide(this: &Dock);
+
+    #[wasm_bindgen(method, js_name = "isVisible")]
+    pub fn is_visible(this: &Dock) -> bool;
+
+    #[wasm_bindgen(method, js_name = "setBadge")]
+    pub fn set_badge(this: &Dock, text: &JsString) -> bool;
+
+    #[wasm_bindgen(method, js_name = "setIcon")]
+    pub fn set_icon(this: &Dock, image: &NativeImage) -> bool;
+
+    #[wasm_bindgen(method, js_name = "setMenu")]
+    pub fn set_menu(this: &Dock, menu: &Menu);
+
+    #[wasm_bindgen(method)]
+    pub fn show(this: &Dock) -> Promise;
 }
