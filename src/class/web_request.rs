885248998da3f24b12aefa5c0ad7725ad4b28c55
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-29 18:59:29 -0700

    Bindings for WebRequest class

--- src/class/web_request.rs
+++ src/class/web_request.rs
@@ -1,4 +1,5 @@
-use js_sys::Object;
+use crate::interface::WebRequestFilter;
+use js_sys::{Function, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,30 @@ extern {
     #[derive(Clone, Debug, Eq, PartialEq)]
     /// Docs: http://electronjs.org/docs/api/web-request
     pub type WebRequest;
+
+    // Instance Methods
+
+    #[wasm_bindgen(method, js_name = "onBeforeRedirect")]
+    pub fn on_before_redirect(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onBeforeRequest")]
+    pub fn on_before_request(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onBeforeSendHeaders")]
+    pub fn on_before_send_headers(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onCompleted")]
+    pub fn on_completed(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onErrorOccurred")]
+    pub fn on_error_occurred(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onHeadersReceived")]
+    pub fn on_headers_received(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onResponseStarted")]
+    pub fn on_response_started(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "onSendHeaders")]
+    pub fn on_send_headers(this: &WebRequest, filter: WebRequestFilter, listener: Option<&Function>);
 }
