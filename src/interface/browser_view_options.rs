1d83f1b0e8a630e536d9d4d24d37f19fcedcd91b
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-02 21:45:34 -0700

    Bindings for BrowserViewOptions interface

--- src/interface/browser_view_options.rs
+++ src/interface/browser_view_options.rs
@@ -1,9 +1,26 @@
+use crate::interface::WebPreferences;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct BrowserViewOptions {}
+#[derive(Clone, Debug)]
+pub struct BrowserViewOptions {
+    web_preferences: Option<WebPreferences>,
+}
 
 #[wasm_bindgen]
 impl BrowserViewOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(web_preferences: Option<WebPreferences>) -> BrowserViewOptions {
+        BrowserViewOptions { web_preferences }
+    }
+
+    #[wasm_bindgen(getter, js_name = "webPreferences")]
+    pub fn web_preferences(&self) -> Option<WebPreferences> {
+        self.web_preferences.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_web_preferences(&mut self, value: Option<WebPreferences>) {
+        self.web_preferences = value;
+    }
 }
