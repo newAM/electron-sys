692952be1dad9fb0692b1e9479b74e97139eec8f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-02 21:30:22 -0700

    Bindings for BluetoothDevice interface

--- src/interface/bluetooth_device.rs
+++ src/interface/bluetooth_device.rs
@@ -1,7 +1,37 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type BluetoothDevice;
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct BluetoothDevice {
+    device_id: JsString,
+    device_name: JsString,
+}
+
+#[wasm_bindgen]
+impl BluetoothDevice {
+    #[wasm_bindgen(constructor)]
+    pub fn new(device_id: JsString, device_name: JsString) -> BluetoothDevice {
+        BluetoothDevice { device_id, device_name }
+    }
+
+    #[wasm_bindgen(getter, js_name = "deviceId")]
+    pub fn device_id(&self) -> JsString {
+        self.device_id.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_device_id(&mut self, value: JsString) {
+        self.device_id = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "deviceName")]
+    pub fn device_name(&self) -> JsString {
+        self.device_name.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_device_name(&mut self, value: JsString) {
+        self.device_name = value;
+    }
 }
