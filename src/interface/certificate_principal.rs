09c9a4bc0aa49a64ca283dade1069f2763be8134
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-02 21:57:27 -0700

    Bindings for CertificatePrincipal interface

--- src/interface/certificate_principal.rs
+++ src/interface/certificate_principal.rs
@@ -1,7 +1,95 @@
+use js_sys::{Array, JsString};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type CertificatePrincipal;
+#[derive(Clone, Debug, PartialEq)]
+pub struct CertificatePrincipal {
+    common_name: JsString,
+    country: JsString,
+    locality: JsString,
+    organization_units: Array,
+    organizations: Array,
+    state: JsString,
+}
+
+#[wasm_bindgen]
+impl CertificatePrincipal {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        common_name: JsString,
+        country: JsString,
+        locality: JsString,
+        organization_units: Array,
+        organizations: Array,
+        state: JsString,
+    ) -> CertificatePrincipal {
+        CertificatePrincipal {
+            common_name,
+            country,
+            locality,
+            organization_units,
+            organizations,
+            state,
+        }
+    }
+
+    #[wasm_bindgen(getter, js_name = "commonName")]
+    pub fn common_name(&self) -> JsString {
+        self.common_name.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_common_name(&mut self, value: JsString) {
+        self.common_name = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn country(&self) -> JsString {
+        self.country.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_country(&mut self, value: JsString) {
+        self.country = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn locality(&self) -> JsString {
+        self.locality.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_locality(&mut self, value: JsString) {
+        self.locality = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "organizationUnits")]
+    pub fn organization_units(&self) -> Array {
+        self.organization_units.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_organization_units(&mut self, value: Array) {
+        self.organization_units = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn organizations(&self) -> Array {
+        self.organizations.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_organizations(&mut self, value: Array) {
+        self.organizations = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn state(&self) -> JsString {
+        self.state.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_state(&mut self, value: JsString) {
+        self.state = value;
+    }
 }
