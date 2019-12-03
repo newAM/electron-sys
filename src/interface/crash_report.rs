1d1be6b1b3987522d0f8f53c3f5ab4371c2afb52
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-02 23:06:27 -0700

    Bindings for CrashReport interface

--- src/interface/crash_report.rs
+++ src/interface/crash_report.rs
@@ -1,7 +1,20 @@
+use js_sys::{Date, JsString};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type CrashReport;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn date(this: &CrashReport) -> Date;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_date(this: &CrashReport, value: Date);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn id(this: &CrashReport) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_id(this: &CrashReport, value: JsString);
 }
