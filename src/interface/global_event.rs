1605e21af93dfdfb75b4ef98719bf71ad50687e1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 09:40:59 -0700

    Bindings for GlobalEvent type

--- src/interface/global_event.rs
+++ src/interface/global_event.rs
@@ -1,9 +1,3 @@
-use wasm_bindgen::prelude::*;
 use web_sys::Event;
 
-#[wasm_bindgen]
-extern {
-    #[wasm_bindgen(extends = Event)]
-    #[derive(Clone, Debug, Eq, PartialEq)]
-    pub type GlobalEvent;
-}
+pub type GlobalEvent = Event;
