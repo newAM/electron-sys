5e2781f685f9aa7526eff4306c31f6398b499d69
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 09:16:45 -0700

    Bindings for ExtensionInfo interface

--- src/interface/extension_info.rs
+++ src/interface/extension_info.rs
@@ -1,7 +1,20 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type ExtensionInfo;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn name(this: &ExtensionInfo) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_name(this: &ExtensionInfo, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn version(this: &ExtensionInfo) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_version(this: &ExtensionInfo, value: JsString);
 }
