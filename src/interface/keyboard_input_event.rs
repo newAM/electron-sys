3dc7bfd78a26d9bc20cf45fa0d5d3bb1e7cec81a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 12:32:29 -0700

    Bindings for KeyboardInputEvent interface

--- src/interface/keyboard_input_event.rs
+++ src/interface/keyboard_input_event.rs
@@ -1,9 +1,52 @@
-use crate::interface::InputEvent;
+use js_sys::{Array, JsString};
 use wasm_bindgen::prelude::*;
 
+// NOTE: extends InputEvent
 #[wasm_bindgen]
-extern {
-    #[wasm_bindgen(extends = InputEvent)]
-    #[derive(Clone, Debug, PartialEq)]
-    pub type KeyboardInputEvent;
+pub struct KeyboardInputEvent {
+    key_code: JsString,
+    kind: JsString,
+    modifiers: Option<Array>,
+}
+
+#[wasm_bindgen]
+impl KeyboardInputEvent {
+    #[wasm_bindgen(constructor)]
+    pub fn new(key_code: JsString, kind: JsString, modifiers: Option<Array>) -> KeyboardInputEvent {
+        KeyboardInputEvent {
+            key_code,
+            kind,
+            modifiers,
+        }
+    }
+
+    #[wasm_bindgen(getter, js_name = "keyCode")]
+    pub fn key_code(&self) -> JsString {
+        self.key_code.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_key_code(&mut self, value: JsString) {
+        self.key_code = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "type")]
+    pub fn kind(&self) -> JsString {
+        self.kind.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_kind(&mut self, value: JsString) {
+        self.kind = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn modifiers(&self) -> Option<Array> {
+        self.modifiers.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_modifiers(&mut self, value: Option<Array>) {
+        self.modifiers = value;
+    }
 }
