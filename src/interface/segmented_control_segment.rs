57685a7a17071430d74c23380562c20acfe1795b
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 19:56:06 -0700

    Bindings for SegmentedControlSegment interface

--- src/interface/segmented_control_segment.rs
+++ src/interface/segmented_control_segment.rs
@@ -1,7 +1,48 @@
+use crate::class::NativeImage;
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type SegmentedControlSegment;
+pub struct SegmentedControlSegment {
+    enabled: Option<bool>,
+    icon: Option<NativeImage>,
+    label: Option<JsString>,
+}
+
+#[wasm_bindgen]
+impl SegmentedControlSegment {
+    #[wasm_bindgen]
+    pub fn new(enabled: Option<bool>, icon: Option<NativeImage>, label: Option<JsString>) -> SegmentedControlSegment {
+        SegmentedControlSegment { enabled, icon, label }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn enabled(&self) -> Option<bool> {
+        self.enabled
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_enabled(&mut self, value: Option<bool>) {
+        self.enabled = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon(&self) -> Option<NativeImage> {
+        self.icon.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon(&mut self, value: Option<NativeImage>) {
+        self.icon = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn label(&self) -> Option<JsString> {
+        self.label.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_label(&mut self, value: Option<JsString>) {
+        self.label = value;
+    }
 }
