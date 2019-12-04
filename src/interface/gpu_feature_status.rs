b3416c0a8e654dac1ccdbc5f9982b3f5c218984e
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 09:50:28 -0700

    Bindings for GpuFeatureStatus interface

--- src/interface/gpu_feature_status.rs
+++ src/interface/gpu_feature_status.rs
@@ -1,3 +1,4 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -5,4 +6,82 @@ extern {
     #[wasm_bindgen(js_name = "GPUFeatureStatus")]
     #[derive(Clone, Debug, PartialEq)]
     pub type GpuFeatureStatus;
+
+    #[wasm_bindgen(method, getter, js_name = "2d_canvas")]
+    pub fn twoD_canvas(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter, js_name = "2d_canvas")]
+    pub fn set_twoD_canvas(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn flash_3d(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_flash_3d(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn flash_stage3d(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_flash_stage3d(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn flash_stage3d_baseline(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_flash_stage3d_baseline(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn gpu_compositing(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_gpu_compositing(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn multiple_raster_threads(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_multiple_raster_threads(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn native_gpu_memory_buffers(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_native_gpu_memory_buffers(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn rasterization(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_rasterization(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn video_decode(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_video_decode(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn video_encode(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_video_encode(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn vpx_decode(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_vpx_decode(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn webgl(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_webgl(this: &GpuFeatureStatus, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn weblg2(this: &GpuFeatureStatus) -> JsString;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_weblg2(this: &GpuFeatureStatus, value: JsString);
 }
