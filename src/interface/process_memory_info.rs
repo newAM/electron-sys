fd196b05d8a9ebd27b9019c1d0410355089321c0
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 01:41:00 -0700

    Bindings for ProcessMemoryInfo interface

--- src/interface/process_memory_info.rs
+++ src/interface/process_memory_info.rs
@@ -4,4 +4,22 @@ use wasm_bindgen::prelude::*;
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type ProcessMemoryInfo;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn private(this: &ProcessMemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_private(this: &ProcessMemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, getter, js_name = "residentSet")]
+    pub fn resident_set(this: &ProcessMemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_resident_set(this: &ProcessMemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn shared(this: &ProcessMemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_shared(this: &ProcessMemoryInfo) -> usize;
 }
