8e60e62f6f1cf434ea73c8e62c728d82373d0df5
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 09:38:11 -0700

    Bindings for FromPartitionOptions interface

--- src/interface/from_partition_options.rs
+++ src/interface/from_partition_options.rs
@@ -1,9 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct FromPartitionOptions {}
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct FromPartitionOptions {
+    cache: bool,
+}
 
 #[wasm_bindgen]
 impl FromPartitionOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(cache: bool) -> FromPartitionOptions {
+        FromPartitionOptions { cache }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn cache(self) -> bool {
+        self.cache
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_cache(mut self, value: bool) {
+        self.cache = value;
+    }
 }
