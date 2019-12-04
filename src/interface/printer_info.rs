a80fd999ae78902ca31a31bac93993b8ba56eef8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 01:36:23 -0700

    Bindings for PrinterInfo interface

--- src/interface/printer_info.rs
+++ src/interface/printer_info.rs
@@ -1,7 +1,20 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type PrinterInfo;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn description(this: &PrinterInfo) -> JsString;
+
+    #[wasm_bindgen(method, getter, js_name = "isDefault")]
+    pub fn is_default(this: &PrinterInfo) -> bool;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn name(this: &PrinterInfo) -> JsString;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn status(this: &PrinterInfo) -> i32;
 }
