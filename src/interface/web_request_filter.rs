5f96f334d2270afc50d18bc4f183ee3e077259b7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 17:33:00 -0700

    Bindings for WebRequestFilter interface

--- src/interface/web_request_filter.rs
+++ src/interface/web_request_filter.rs
@@ -1,9 +1,26 @@
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct WebRequestFilter {}
+pub struct WebRequestFilter {
+    urls: Array,
+}
 
 #[wasm_bindgen]
 impl WebRequestFilter {
+    #[wasm_bindgen(constructor)]
+    pub fn new(urls: Array) -> WebRequestFilter {
+        WebRequestFilter { urls }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn urls(&self) -> Array {
+        self.urls.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_urls(&mut self, value: Array) {
+        self.urls = value;
+    }
 }
