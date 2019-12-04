58f9a12f1f206be5fe7bc33ae02b8466d8c99d9d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 11:02:25 -0700

    Bindings for IoCounters interface

--- src/interface/io_counters.rs
+++ src/interface/io_counters.rs
@@ -2,7 +2,42 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    #[wasm_bindgen(js_name = "IOCounters")]
     #[derive(Clone, Debug, PartialEq)]
     pub type IoCounters;
+
+    #[wasm_bindgen(method, getter, js_name = "otherOperationCount")]
+    pub fn other_operation_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "otherOperationCount")]
+    pub fn set_other_operation_count(this: &IoCounters, value: usize);
+
+    #[wasm_bindgen(method, getter, js_name = "otherTransferCount")]
+    pub fn other_transfer_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "otherTransferCount")]
+    pub fn set_other_transfer_count(this: &IoCounters, value: usize);
+
+    #[wasm_bindgen(method, getter, js_name = "readOperationCount")]
+    pub fn read_operation_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "readOperationCount")]
+    pub fn set_read_operation_count(this: &IoCounters, value: usize);
+
+    #[wasm_bindgen(method, getter, js_name = "readTransferCount")]
+    pub fn read_transfer_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "readTransferCount")]
+    pub fn set_read_transfer_count(this: &IoCounters, value: usize);
+
+    #[wasm_bindgen(method, getter, js_name = "writeOperationCount")]
+    pub fn write_operation_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "writeOperationCount")]
+    pub fn set_write_operation_count(this: &IoCounters, value: usize);
+
+    #[wasm_bindgen(method, getter, js_name = "writeTransferCount")]
+    pub fn write_transfer_count(this: &IoCounters) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "writeTransferCount")]
+    pub fn set_write_transfer_count(this: &IoCounters, value: usize);
 }
