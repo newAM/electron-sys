2b43bb39fa12b2bda7dec1788d85d67111cdd44f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 08:38:22 -0700

    Bindings for CustomScheme interface

--- src/interface/custom_scheme.rs
+++ src/interface/custom_scheme.rs
@@ -1,7 +1,38 @@
+use crate::interface::Privileges;
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type CustomScheme;
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CustomScheme {
+    privileges: Option<Privileges>,
+    scheme: JsString,
+}
+
+#[wasm_bindgen]
+impl CustomScheme {
+    #[wasm_bindgen(constructor)]
+    pub fn new(privileges: Option<Privileges>, scheme: JsString) -> CustomScheme {
+        CustomScheme { privileges, scheme }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn privileges(&self) -> Option<Privileges> {
+        self.privileges
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_privileges(&mut self, value: Option<Privileges>) {
+        self.privileges = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn scheme(&self) -> JsString {
+        self.scheme.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_scheme(&mut self, value: JsString) {
+        self.scheme = value;
+    }
 }
