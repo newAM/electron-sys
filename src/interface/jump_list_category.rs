53ce3a425ead7d393332320193c85cbfe4d8866d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 11:30:38 -0700

    Bindings for JumpListCategory interface

--- src/interface/jump_list_category.rs
+++ src/interface/jump_list_category.rs
@@ -1,7 +1,47 @@
+use js_sys::{Array, JsString};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type JumpListCategory;
+pub struct JumpListCategory {
+    items: Option<Array>,
+    kind: Option<JsString>,
+    name: Option<JsString>,
+}
+
+#[wasm_bindgen]
+impl JumpListCategory {
+    #[wasm_bindgen(constructor)]
+    pub fn new(items: Option<Array>, kind: Option<JsString>, name: Option<JsString>) -> JumpListCategory {
+        JumpListCategory { items, kind, name }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn items(&self) -> Option<Array> {
+        self.items.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_items(&mut self, value: Option<Array>) {
+        self.items = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "type")]
+    pub fn kind(&self) -> Option<JsString> {
+        self.kind.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_kind(&mut self, value: Option<JsString>) {
+        self.kind = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn name(&self) -> Option<JsString> {
+        self.name.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_name(&mut self, value: Option<JsString>) {
+        self.name = value;
+    }
 }
