7385b1690a8af3568c823cefbdf81725b49c2608
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 01:12:04 -0700

    Binding for Point interface

--- src/interface/point.rs
+++ src/interface/point.rs
@@ -1,7 +1,36 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type Point;
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+
+#[wasm_bindgen]
+impl Point {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: usize, y: usize) -> Point {
+        Point { x, y }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn x(self) -> usize {
+        self.x
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_x(mut self, value: usize) {
+        self.y = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(self) -> usize {
+        self.y
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_y(mut self, value: usize) {
+        self.y = value;
+    }
 }
