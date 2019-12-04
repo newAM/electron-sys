d81457d071f4af11e036da4a28c51b3dec366dde
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 09:27:21 -0700

    Bindings for FilePathWithHeaders interface

--- src/interface/file_path_with_headers.rs
+++ src/interface/file_path_with_headers.rs
@@ -1,7 +1,36 @@
+use js_sys::{JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type FilePathWithHeaders;
+pub struct FilePathWithHeaders {
+    headers: Option<Object>,
+    path: JsString,
+}
+
+#[wasm_bindgen]
+impl FilePathWithHeaders {
+    #[wasm_bindgen(constructor)]
+    pub fn new(headers: Option<Object>, path: JsString) -> FilePathWithHeaders {
+        FilePathWithHeaders { headers, path }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn headers(&self) -> Option<Object> {
+        self.headers.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_headers(&mut self, value: Option<Object>) {
+        self.headers = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn path(&self) -> JsString {
+        self.path.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_path(&mut self, value: JsString) {
+        self.path = value;
+    }
 }
