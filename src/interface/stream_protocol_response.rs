829b9c6ac0cdd35a0bf759a6e352ac04cd6ab5c2
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 19:09:37 -0700

    Bindings for StreamProtocolResponse interface

--- src/interface/stream_protocol_response.rs
+++ src/interface/stream_protocol_response.rs
@@ -1,7 +1,56 @@
+use js_sys::Object;
+use node_sys::ReadableStream;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type StreamProtocolReponse;
+pub struct StreamProtocolResponse {
+    data: Option<ReadableStream>,
+    headers: Option<Object>,
+    status_code: Option<usize>,
+}
+
+#[wasm_bindgen]
+impl StreamProtocolResponse {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        data: Option<ReadableStream>,
+        headers: Option<Object>,
+        status_code: Option<usize>,
+    ) -> StreamProtocolResponse {
+        StreamProtocolResponse {
+            data,
+            headers,
+            status_code,
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn data(&self) -> Option<ReadableStream> {
+        self.data.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_data(&mut self, value: Option<ReadableStream>) {
+        self.data = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn headers(&self) -> Option<Object> {
+        self.headers.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_headers(&mut self, value: Option<Object>) {
+        self.headers = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "statusCode")]
+    pub fn status_code(&self) -> Option<usize> {
+        self.status_code
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_status_code(&mut self, value: Option<usize>) {
+        self.status_code = value;
+    }
 }
