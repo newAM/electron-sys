e00346783d24dbeaf02708942ec66e7823738afe
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 11:26:32 -0700

    Bindings for Item interface

--- src/interface/item.rs
+++ src/interface/item.rs
@@ -1,9 +1,38 @@
+use crate::class::NativeImage;
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Item {}
+pub struct Item {
+    file: Array,
+    icon: NativeImage,
+}
 
 #[wasm_bindgen]
 impl Item {
+    #[wasm_bindgen(constructor)]
+    pub fn new(file: Array, icon: NativeImage) -> Item {
+        Item { file, icon }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn file(&self) -> Array {
+        self.file.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_file(&mut self, value: Array) {
+        self.file = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon(&self) -> NativeImage {
+        self.icon.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon(&mut self, value: NativeImage) {
+        self.icon = value;
+    }
 }
