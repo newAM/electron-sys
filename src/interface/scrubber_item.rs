6a70738259e286344a89c2c15cb573b562dae9c2
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 21:34:22 -0700

    Bindings for ScrubberItem interface

--- src/interface/scrubber_item.rs
+++ src/interface/scrubber_item.rs
@@ -1,7 +1,38 @@
+use crate::class::NativeImage;
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type ScrubberItem;
+#[derive(Clone, Debug, PartialEq)]
+pub struct ScrubberItem {
+    icon: Option<NativeImage>,
+    label: Option<JsString>,
+}
+
+#[wasm_bindgen]
+impl ScrubberItem {
+    #[wasm_bindgen(constructor)]
+    pub fn new(icon: Option<NativeImage>, label: Option<JsString>) -> ScrubberItem {
+        ScrubberItem { icon, label }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon(&self) -> Option<NativeImage> {
+        self.icon.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon(&mut self, value: Option<NativeImage>) {
+        self.icon = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn label(&self) -> Option<JsString> {
+        self.label.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_label(&mut self, value: Option<JsString>) {
+        self.label = value;
+    }
 }
