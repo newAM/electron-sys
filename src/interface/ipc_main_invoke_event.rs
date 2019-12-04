7a65599b1264605bf3bcc62b701ea071d27e9914
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 11:18:58 -0700

    Bindings for IpcMainInvokeEvent interface

--- src/interface/ipc_main_invoke_event.rs
+++ src/interface/ipc_main_invoke_event.rs
@@ -1,3 +1,4 @@
+use crate::class::WebContents;
 use wasm_bindgen::prelude::*;
 use web_sys::Event;
 
@@ -6,4 +7,16 @@ extern {
     #[wasm_bindgen(extends = Event)]
     #[derive(Clone, Debug, PartialEq)]
     pub type IpcMainInvokeEvent;
+
+    #[wasm_bindgen(method, getter, js_name = "frameId")]
+    pub fn frame_id(this: &IpcMainInvokeEvent) -> u32;
+
+    #[wasm_bindgen(method, setter, js_name = "frameId")]
+    pub fn set_frame_id(this: &IpcMainInvokeEvent, value: u32);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn sender(this: &IpcMainInvokeEvent) -> WebContents;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_sender(this: &IpcMainInvokeEvent, value: WebContents);
 }
