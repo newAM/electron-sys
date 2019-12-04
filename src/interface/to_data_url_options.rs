c67fdf70457552ed156c3cf69d909f14166883b8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 23:35:21 -0700

    Bindings for ToDataUrlOptions interface

--- src/interface/to_data_url_options.rs
+++ src/interface/to_data_url_options.rs
@@ -1,9 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ToDataUrlOptions {}
+#[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
+pub struct ToDataUrlOptions {
+    scale_factor: Option<f32>,
+}
 
 #[wasm_bindgen]
 impl ToDataUrlOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(scale_factor: Option<f32>) -> ToDataUrlOptions {
+        ToDataUrlOptions { scale_factor }
+    }
+
+    #[wasm_bindgen(getter, js_name = "scaleFactor")]
+    pub fn scale_factor(self) -> Option<f32> {
+        self.scale_factor
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_scale_factor(mut self, value: Option<f32>) {
+        self.scale_factor = value;
+    }
 }
