715287956948b618f0ae14ca7b4f235ef4341318
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 13:53:18 -0700

    Bindings for Referrer interface

--- src/interface/referrer.rs
+++ src/interface/referrer.rs
@@ -1,7 +1,37 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type Referrer;
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Referrer {
+    policy: JsString,
+    url: JsString,
+}
+
+#[wasm_bindgen]
+impl Referrer {
+    #[wasm_bindgen(constructor)]
+    pub fn new(policy: JsString, url: JsString) -> Referrer {
+        Referrer { policy, url }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn policy(&self) -> JsString {
+        self.policy.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_policy(&mut self, value: JsString) {
+        self.policy = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn url(&self) -> JsString {
+        self.url.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_url(&mut self, value: JsString) {
+        self.url = value;
+    }
 }
