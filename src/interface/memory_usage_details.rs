42dde2d5ff0a1acfc1f220d3cf1e1a815afe9c75
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 15:14:28 -0700

    Bindings for MemoryUsageDetails interface

--- src/interface/memory_usage_details.rs
+++ src/interface/memory_usage_details.rs
@@ -4,4 +4,22 @@ use wasm_bindgen::prelude::*;
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type MemoryUsageDetails;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn count(this: &MemoryUsageDetails) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_count(this: &MemoryUsageDetails) -> usize;
+
+    #[wasm_bindgen(method, getter, js_name = "liveSize")]
+    pub fn live_size(this: &MemoryUsageDetails) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_live_size(this: &MemoryUsageDetails) -> usize;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn size(this: &MemoryUsageDetails) -> usize;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_size(this: &MemoryUsageDetails) -> usize;
 }
