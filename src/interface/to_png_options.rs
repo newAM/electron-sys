9e64e24634dc20e22dfd4ad6add69602cd0e8260
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 23:36:57 -0700

    Bindings for ToPngOptions interface

--- src/interface/to_png_options.rs
+++ src/interface/to_png_options.rs
@@ -1,9 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ToPngOptions {}
+#[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
+pub struct ToPngOptions {
+    scale_factor: Option<f32>,
+}
 
 #[wasm_bindgen]
 impl ToPngOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(scale_factor: Option<f32>) -> ToPngOptions {
+        ToPngOptions { scale_factor }
+    }
+
+    #[wasm_bindgen(getter, js_name = "scaleFactor")]
+    pub fn scale_factor(self) -> Option<f32> {
+        self.scale_factor
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_scale_factor(mut self, value: Option<f32>) {
+        self.scale_factor = value;
+    }
 }
