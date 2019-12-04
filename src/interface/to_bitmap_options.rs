672993b5cc987f84b22f52d0de369091865103cd
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 23:33:42 -0700

    Bindings for ToBitmapOptions interface

--- src/interface/to_bitmap_options.rs
+++ src/interface/to_bitmap_options.rs
@@ -1,9 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ToBitmapOptions {}
+#[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
+pub struct ToBitmapOptions {
+    scale_factor: Option<f32>,
+}
 
 #[wasm_bindgen]
 impl ToBitmapOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(scale_factor: Option<f32>) -> ToBitmapOptions {
+        ToBitmapOptions { scale_factor }
+    }
+
+    #[wasm_bindgen(getter, js_name = "scaleFactor")]
+    pub fn scale_factor(self) -> Option<f32> {
+        self.scale_factor
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_scale_factor(mut self, value: Option<f32>) {
+        self.scale_factor = value;
+    }
 }
