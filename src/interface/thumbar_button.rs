d88120aea6a785f2e2f345041c998954b9a0994d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 23:27:51 -0700

    Bindings for ThumbarButton interface

--- src/interface/thumbar_button.rs
+++ src/interface/thumbar_button.rs
@@ -1,7 +1,64 @@
+use crate::class::NativeImage;
+use js_sys::{Array, Function};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type ThumbarButton;
+pub struct ThumbarButton {
+    click: Function,
+    flags: Option<Array>,
+    icon: NativeImage,
+    tooltip: Option<Array>,
+}
+
+#[wasm_bindgen]
+impl ThumbarButton {
+    #[wasm_bindgen]
+    pub fn new(click: Function, flags: Option<Array>, icon: NativeImage, tooltip: Option<Array>) -> ThumbarButton {
+        ThumbarButton {
+            click,
+            flags,
+            icon,
+            tooltip,
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn click(&self) -> Function {
+        self.click.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_click(&mut self, value: Function) {
+        self.click = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn flags(&self) -> Option<Array> {
+        self.flags.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_flags(&mut self, value: Option<Array>) {
+        self.flags = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn icon(&self) -> NativeImage {
+        self.icon.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_icon(&mut self, value: NativeImage) {
+        self.icon = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn tooltip(&self) -> Option<Array> {
+        self.tooltip.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_tooltip(&mut self, value: Option<Array>) {
+        self.tooltip = value;
+    }
 }
