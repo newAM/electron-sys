133539087801fe03aea3aecadc272e0427ef3da3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 02:06:40 -0700

    Bindings for CpuUsage interface

--- src/interface/cpu_usage.rs
+++ src/interface/cpu_usage.rs
@@ -2,7 +2,18 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    #[wasm_bindgen(js_name = "CPUUsage")]
     #[derive(Clone, Debug, PartialEq)]
     pub type CpuUsage;
+
+    #[wasm_bindgen(method, getter, js_name = "idleWakeupsPerSecond")]
+    pub fn idle_wakeups_per_second(this: &CpuUsage) -> u32;
+
+    #[wasm_bindgen(method, setter, js_name = "idleWakeupsPerSecond")]
+    pub fn set_idle_wakeups_per_second(this: &CpuUsage, value: u32);
+
+    #[wasm_bindgen(method, getter, js_name = "percentCPUUsage")]
+    pub fn percent_cpu_usage(this: &CpuUsage) -> u32;
+
+    #[wasm_bindgen(method, setter, js_name = "percentCPUUsage")]
+    pub fn set_percent_cpu_usage(this: &CpuUsage, value: u32);
 }
