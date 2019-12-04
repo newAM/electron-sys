f36993ec0dd51b02591f02fd07dffc9ddd3606a0
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 15:09:41 -0700

    Bindings for MemoryInfo interface

--- src/interface/memory_info.rs
+++ src/interface/memory_info.rs
@@ -4,4 +4,22 @@ use wasm_bindgen::prelude::*;
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type MemoryInfo;
+
+    #[wasm_bindgen(method, getter, js_name = "peakWorkingSetSize")]
+    pub fn peak_working_set_size(this: &MemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "peakWorkingSetSize")]
+    pub fn set_peak_working_set_size(this: &MemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, getter, js_name = "privateBytes")]
+    pub fn private_bytes(this: &MemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "privateBytes")]
+    pub fn set_private_bytes(this: &MemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, getter, js_name = "workingSetSize")]
+    pub fn working_set_size(this: &MemoryInfo) -> usize;
+
+    #[wasm_bindgen(method, setter, js_name = "workingSetSize")]
+    pub fn set_working_set_size(this: &MemoryInfo) -> usize;
 }
