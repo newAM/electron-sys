ed3183d1eb39bf69d7afb597074857c56e580c65
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-03 17:40:00 -0700

    Bindings for UploadRawData interface

--- src/interface/upload_raw_data.rs
+++ src/interface/upload_raw_data.rs
@@ -1,7 +1,37 @@
+use js_sys::JsString;
+use node_sys::Buffer;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type UploadRawData;
+pub struct UploadRawData {
+    bytes: Buffer,
+    kind: JsString,
+}
+
+#[wasm_bindgen]
+impl UploadRawData {
+    #[wasm_bindgen(constructor)]
+    pub fn new(bytes: Buffer, kind: JsString) -> UploadRawData {
+        UploadRawData { bytes, kind }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn bytes(&self) -> Buffer {
+        self.bytes.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_bytes(&mut self, value: Buffer) {
+        self.bytes = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "type")]
+    pub fn kind(&self) -> JsString {
+        self.kind.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_kind(&mut self, value: JsString) {
+        self.kind = value;
+    }
 }
