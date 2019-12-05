e98289e93297744c772eb8e1b06ca0dd6ae57143
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-05 13:43:55 -0700

    Bindings for contentTracing module

--- src/module/content_tracing.rs
+++ src/module/content_tracing.rs
@@ -1,3 +1,4 @@
+use js_sys::Promise;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,16 @@ extern {
 
     #[wasm_bindgen(js_name = "contentTracing")]
     pub static content_tracing: ContentTracing;
+
+    #[wasm_bindgen(method, js_name = "getCategories")]
+    pub fn get_categories(this: &ContentTracing) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "getTraceBufferUsage")]
+    pub fn get_trace_buffer_usage(this: &ContentTracing) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "startRecording")]
+    pub fn start_recording(this: &ContentTracing) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "stopRecording")]
+    pub fn stop_recording(this: &ContentTracing) -> Promise;
 }
