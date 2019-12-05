e27bb448636ef8557870b1b39e29a7b14d4b47a0
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 23:48:14 -0700

    Bindings for TraceCategoriesAndOptions interface

--- src/interface/trace_categories_and_options.rs
+++ src/interface/trace_categories_and_options.rs
@@ -1,7 +1,40 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type TraceCategoriesAndOptions;
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TraceCategoriesAndOptions {
+    category_filter: JsString,
+    trace_options: JsString,
+}
+
+#[wasm_bindgen]
+impl TraceCategoriesAndOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(category_filter: JsString, trace_options: JsString) -> TraceCategoriesAndOptions {
+        TraceCategoriesAndOptions {
+            category_filter,
+            trace_options,
+        }
+    }
+
+    #[wasm_bindgen(getter, js_name = "categoryFilter")]
+    pub fn category_filter(&self) -> JsString {
+        self.category_filter.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_category_filter(&mut self, value: JsString) {
+        self.category_filter = value;
+    }
+
+    #[wasm_bindgen(getter, js_name = "traceOptions")]
+    pub fn trace_options(&self) -> JsString {
+        self.trace_options.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_trace_options(&mut self, value: JsString) {
+        self.trace_options = value;
+    }
 }
