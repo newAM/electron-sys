40ef52f3498571ca59047afef5c07869d57d07b3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 23:12:51 -0700

    Bindings for ProtocolResponseUploadData interface

--- src/interface/protocol_response_upload_data.rs
+++ src/interface/protocol_response_upload_data.rs
@@ -1,7 +1,20 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type ProtocolResponseUploadData;
+
+    #[wasm_bindgen(method, getter, js_name = "contentType")]
+    pub fn content_type(this: &ProtocolResponseUploadData) -> JsString;
+
+    #[wasm_bindgen(method, setter, js_name = "contentType")]
+    pub fn set_content_type(this: &ProtocolResponseUploadData, value: JsString);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn data(this: &ProtocolResponseUploadData) -> JsValue;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_data(this: &ProtocolResponseUploadData, value: JsValue);
 }
