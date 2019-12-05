7d13d0af3a63c48380cf197156be90929c8b94db
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-05 14:06:18 -0700

    Bindings for crashReporter module

--- src/module/crash_reporter.rs
+++ src/module/crash_reporter.rs
@@ -1,3 +1,5 @@
+use crate::interface::{CrashReport, CrashReporterStartOptions};
+use js_sys::{Array, JsString};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +9,31 @@ extern {
 
     #[wasm_bindgen(js_name = "crashReporter")]
     pub static crash_reporter: CrashReporter;
+
+    #[wasm_bindgen(method, js_name = "addExtraParameter")]
+    pub fn add_extra_parameter(this: &CrashReporter, key: &JsString, value: &JsString);
+
+    #[wasm_bindgen(method, js_name = "getCrashesDirectory")]
+    pub fn get_crashes_directory(this: &CrashReporter) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "getLastCrashReport")]
+    pub fn get_last_crash_report(this: &CrashReporter) -> CrashReport;
+
+    #[wasm_bindgen(method, js_name = "getParameters")]
+    pub fn get_parameters(this: &CrashReporter);
+
+    #[wasm_bindgen(method, js_name = "getUploadedReports")]
+    pub fn get_uploaded_reports(this: &CrashReporter) -> Array;
+
+    #[wasm_bindgen(method, js_name = "getUploadToServer")]
+    pub fn get_upload_to_server(this: &CrashReporter) -> bool;
+
+    #[wasm_bindgen(method, js_name = "removeExtraParameter")]
+    pub fn remove_extra_parameter(this: &CrashReporter, key: &JsString);
+
+    #[wasm_bindgen(method, js_name = "setUploadToServer")]
+    pub fn setUploadToServer(this: &CrashReporter, upload_to_server: bool);
+
+    #[wasm_bindgen(method)]
+    pub fn start(this: &CrashReporter, options: CrashReporterStartOptions);
 }
