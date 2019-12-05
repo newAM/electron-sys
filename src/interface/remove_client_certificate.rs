2f0db1dec1636319b6cbd02f4d85b8cdfafb5dfc
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 23:28:48 -0700

    Bindings for RemoveClientCertificate interface

--- src/interface/remove_client_certificate.rs
+++ src/interface/remove_client_certificate.rs
@@ -1,7 +1,37 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type RemoveClientCertificate;
+#[derive(Clone, Debug, PartialEq)]
+pub struct RemoveClientCertificate {
+    kind: JsString,
+    origin: JsString,
+}
+
+#[wasm_bindgen]
+impl RemoveClientCertificate {
+    #[wasm_bindgen(constructor)]
+    pub fn new(kind: JsString, origin: JsString) -> RemoveClientCertificate {
+        RemoveClientCertificate { kind, origin }
+    }
+
+    #[wasm_bindgen(getter, js_name = "type")]
+    pub fn kind(&self) -> JsString {
+        self.kind.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_kind(&mut self, value: JsString) {
+        self.kind = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn origin(&self) -> JsString {
+        self.origin.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_origin(&mut self, value: JsString) {
+        self.origin = value;
+    }
 }
