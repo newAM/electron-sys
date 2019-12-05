34491e3d8756bf7552bc748a7d4012cc9eb4d8b5
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-05 16:49:41 -0700

    Bindings for dialog module

--- src/module/dialog.rs
+++ src/module/dialog.rs
@@ -1,3 +1,16 @@
+use crate::{
+    class::BrowserWindow,
+    interface::{
+        CertificateTrustDialogOptions,
+        MessageBoxOptions,
+        MessageBoxSyncOptions,
+        OpenDialogOptions,
+        OpenDialogSyncOptions,
+        SaveDialogOptions,
+        SaveDialogSyncOptions,
+    },
+};
+use js_sys::{Array, JsString, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -6,4 +19,56 @@ extern {
     pub type Dialog;
 
     pub static dialog: Dialog;
+
+    #[wasm_bindgen(method, js_name = "showCertificateTrustDialog")]
+    pub fn show_certificate_trust_dialog(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>, // FIXME: verify this works with Option<_> for overloads
+        options: CertificateTrustDialogOptions,
+    ) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "showErrBox")]
+    pub fn show_err_box(this: &Dialog, title: &JsString, content: &JsString);
+
+    #[wasm_bindgen(method, js_name = "showMessageBox")]
+    pub fn show_message_box(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: MessageBoxOptions,
+    ) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "showMessageBoxSync")]
+    pub fn show_message_box_sync(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: MessageBoxSyncOptions,
+    ) -> usize;
+
+    #[wasm_bindgen(method, js_name = "showOpenDialog")]
+    pub fn show_open_dialog(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: OpenDialogOptions,
+    ) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "showOpenDialogSync")]
+    pub fn show_open_dialog_sync(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: OpenDialogSyncOptions,
+    ) -> Option<Array>;
+
+    #[wasm_bindgen(method, js_name = "showSaveDialog")]
+    pub fn show_save_dialog(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: SaveDialogOptions,
+    ) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "showSaveDialogSync")]
+    pub fn show_save_dialog_sync(
+        this: &Dialog,
+        browser_window: Option<&BrowserWindow>,
+        options: SaveDialogSyncOptions,
+    ) -> Option<JsString>;
 }
