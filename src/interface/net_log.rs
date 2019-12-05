965f7944e6bc7f3e8687fdc7a79f6c3b57b13717
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-04 19:12:27 -0700

    Bindings for NetLog interface

--- src/interface/net_log.rs
+++ src/interface/net_log.rs
@@ -1,9 +1,29 @@
+use crate::interface::StartLoggingOptions;
+use js_sys::{JsString, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct NetLog {}
+extern {
+    #[derive(Clone, Debug, PartialEq)]
+    pub type NetLog;
 
-#[wasm_bindgen]
-impl NetLog {
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "startLogging")]
+    pub fn start_logging(this: &NetLog, path: &JsString, options: StartLoggingOptions) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "stopLogging")]
+    pub fn stop_logging(this: &NetLog) -> Promise;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter, js_name = "currentlyLogging")]
+    pub fn currently_logging(this: &NetLog) -> bool;
+
+    #[wasm_bindgen(method, getter, js_name = "currentlyLoggingPath")]
+    pub fn currently_logging_path(this: &NetLog) -> JsString;
 }
