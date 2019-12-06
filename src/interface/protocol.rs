57451cbe3f76bd819ab8affd19d81aaa80047229
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 07:00:44 -0700

    Bindings for Protocol interface

--- src/interface/protocol.rs
+++ src/interface/protocol.rs
@@ -1,9 +1,100 @@
+use js_sys::{Array, Function, JsString, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Protocol {}
+extern {
+    #[derive(Clone, Debug)]
+    pub type Protocol;
 
-#[wasm_bindgen]
-impl Protocol {
+    #[wasm_bindgen(method, js_name = "interceptBufferProtocol")]
+    pub fn intercept_buffer_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "interceptFileProtocol")]
+    pub fn intercept_file_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "interceptHttpProtocol")]
+    pub fn intercept_http_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "interceptStreamProtocol")]
+    pub fn intercept_stream_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "interceptStringProtocol")]
+    pub fn intercept_string_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "isProtocolHandled")]
+    pub fn is_protocol_handled(this: &Protocol, scheme: &JsString) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "registerBufferProtocol")]
+    pub fn register_buffer_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "registerFileProtocol")]
+    pub fn register_file_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "registerHttpProtocol")]
+    pub fn register_http_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "registerSchemesAsPriviledged")]
+    pub fn register_schemes_as_privileged(this: &Protocol, custom_schemes: &Array);
+
+    #[wasm_bindgen(method, js_name = "registerStreamProtocol")]
+    pub fn register_stream_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "registerStringProtocol")]
+    pub fn register_string_protocol(
+        this: &Protocol,
+        scheme: &JsString,
+        handler: &Function,
+        completion: Option<&Function>,
+    );
+
+    #[wasm_bindgen(method, js_name = "uninterceptProtocol")]
+    pub fn unintercept_protocol(this: &Protocol, scheme: &JsString, completion: Option<&Function>);
+
+    #[wasm_bindgen(method, js_name = "unregisterProtocol")]
+    pub fn unregister_protocl(this: &Protocol, scheme: &JsString, completion: Option<&Function>);
 }
