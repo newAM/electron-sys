42b63b636a3a6863c8193c4b1aaac610b36298f4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 07:28:40 -0700

    Bindings for CertificateTrust interface

--- src/interface/certificate_trust_dialog_options.rs
+++ src/interface/certificate_trust_dialog_options.rs
@@ -1,7 +1,38 @@
+use crate::interface::Certificate;
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    #[derive(Clone, Debug, PartialEq)]
-    pub type CertificateTrustDialogOptions;
+#[derive(Clone, Debug, PartialEq)]
+pub struct CertificateTrustDialogOptions {
+    certificate: Certificate,
+    message: JsString,
+}
+
+#[wasm_bindgen]
+impl CertificateTrustDialogOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new(certificate: Certificate, message: JsString) -> CertificateTrustDialogOptions {
+        CertificateTrustDialogOptions { certificate, message }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn certificate(&self) -> Certificate {
+        self.certificate.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_certificate(&mut self, value: Certificate) {
+        self.certificate = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn message(&self) -> JsString {
+        self.message.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_message(&mut self, value: JsString) {
+        self.message = value;
+    }
 }
