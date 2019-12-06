d36fc413d1c5d8895ef9d764f953143c24c6f0c1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 10:05:03 -0700

    Bindings for TouchBarOptions interface

--- src/interface/touch_bar_options.rs
+++ src/interface/touch_bar_options.rs
@@ -1,9 +1,41 @@
+use js_sys::{Array, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct TouchBarOptions {}
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct TouchBarOptions {
+    escape_item: Option<Object>,
+    items: Option<Array>,
+}
 
 #[wasm_bindgen]
 impl TouchBarOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new_with_values(escape_item: Option<Object>, items: Option<Array>) -> TouchBarOptions {
+        TouchBarOptions { escape_item, items }
+    }
+
+    pub fn new() -> TouchBarOptions {
+        Default::default()
+    }
+
+    #[wasm_bindgen(getter, js_name = "escapeItem")]
+    pub fn escape_item(&self) -> Option<Object> {
+        self.escape_item.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_escape_item(&mut self, value: Option<Object>) {
+        self.escape_item = value;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn items(&self) -> Option<Array> {
+        self.items.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_items(&mut self, value: Option<Array>) {
+        self.items = value;
+    }
 }
