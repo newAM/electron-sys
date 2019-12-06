980436edb5965a5858576e574c89645058207068
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 10:05:51 -0700

    Bindings for TouchBarSpacerOptions interface

--- src/interface/touch_bar_spacer_options.rs
+++ src/interface/touch_bar_spacer_options.rs
@@ -1,9 +1,30 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct TouchBarSpacerOptions {}
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct TouchBarSpacerOptions {
+    size: Option<JsString>,
+}
 
 #[wasm_bindgen]
 impl TouchBarSpacerOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new_with_values(size: Option<JsString>) -> TouchBarSpacerOptions {
+        TouchBarSpacerOptions { size }
+    }
+
+    pub fn new() -> TouchBarSpacerOptions {
+        Default::default()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn size(&self) -> Option<JsString> {
+        self.size.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_size(&mut self, value: Option<JsString>) {
+        self.size = value;
+    }
 }
