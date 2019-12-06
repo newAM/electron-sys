1232787d24434933bb8c304b8b059c468a7ed4f7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 07:16:56 -0700

    Bindings for MessageBoxReturnValue interface

--- src/interface/message_box_return_value.rs
+++ src/interface/message_box_return_value.rs
@@ -4,4 +4,16 @@ use wasm_bindgen::prelude::*;
 extern {
     #[derive(Clone, Debug, PartialEq)]
     pub type MessageBoxReturnValue;
+
+    #[wasm_bindgen(method, getter, js_name = checkboxChecked)]
+    pub fn checkbox_checked(this: &MessageBoxReturnValue) -> bool;
+
+    #[wasm_bindgen(method, setter, js_name = checkboxChecked)]
+    pub fn set_checkbox_checked(this: &MessageBoxReturnValue, value: bool);
+
+    #[wasm_bindgen(method, getter)]
+    pub fn response(this: &MessageBoxReturnValue) -> u32;
+
+    #[wasm_bindgen(method, setter)]
+    pub fn set_esponse(this: &MessageBoxReturnValue, value: u32);
 }
