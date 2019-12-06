65bff93361cdddde1728f0faa288d39cf4158c9f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 09:15:45 -0700

    Add missing method and getter attributes

--- src/interface/payment.rs
+++ src/interface/payment.rs
@@ -12,7 +12,9 @@ extern {
     #[wasm_bindgen(method, setter, js_name = "productIdentifier")]
     pub fn set_product_identifier(this: &Payment, value: JsString);
 
+    #[wasm_bindgen(method, getter)]
     pub fn quantity(this: &Payment) -> u32;
 
+    #[wasm_bindgen(method, setter)]
     pub fn set_quantity(this: &Payment, value: u32);
 }
