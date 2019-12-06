bda60f3894c3df8809f2859b2f66e8cd953263c3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 15:21:20 -0700

    Bindings for inAppPurchase module

--- src/module/in_app_purchase.rs
+++ src/module/in_app_purchase.rs
@@ -1,3 +1,4 @@
+use js_sys::{Array, JsString, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,28 @@ extern {
 
     #[wasm_bindgen(js_name = "inAppPurchase")]
     pub static in_app_purchase: InAppPurchase;
+
+    // FIXME: event overloads
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "canMakePayments")]
+    pub fn can_make_payments(this: &InAppPurchase) -> bool;
+
+    #[wasm_bindgen(method, js_name = "finishAllTransactions")]
+    pub fn finish_all_transactions(this: &InAppPurchase);
+
+    #[wasm_bindgen(method, js_name = "finishTransactionsByDate")]
+    pub fn finish_transactions_by_date(this: &InAppPurchase, date: &JsString);
+
+    #[wasm_bindgen(method, js_name = "getProducts")]
+    pub fn get_products(this: &InAppPurchase, product_ids: &Array) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "getReceiptsURL")]
+    pub fn get_receipts_url(this: &InAppPurchase) -> JsString;
+
+    #[wasm_bindgen(method, js_name = "purchaseProduct")]
+    pub fn purchaseProduct(this: &InAppPurchase, product_id: &JsString, quantity: Option<u32>) -> Promise;
 }
