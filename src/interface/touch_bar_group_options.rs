ad8163c55ed385c65a4789b3fbc414165e179f3d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 10:04:47 -0700

    Bindings for TouchBarGroupOptions interface

--- src/interface/touch_bar_group_options.rs
+++ src/interface/touch_bar_group_options.rs
@@ -1,9 +1,26 @@
+use crate::class::TouchBar;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct TouchBarGroupOptions {}
+pub struct TouchBarGroupOptions {
+    items: TouchBar,
+}
 
 #[wasm_bindgen]
 impl TouchBarGroupOptions {
+    #[wasm_bindgen]
+    pub fn new(items: TouchBar) -> TouchBarGroupOptions {
+        TouchBarGroupOptions { items }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn items(&self) -> TouchBar {
+        self.items.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_items(&mut self, value: TouchBar) {
+        self.items = value;
+    }
 }
