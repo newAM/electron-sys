6ddb070bb0396ce50bf5c597e93ff7d059f0d6bf
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 20:11:29 -0700

    Bindings for ipcRenderer module

--- src/module/ipc_renderer.rs
+++ src/module/ipc_renderer.rs
@@ -1,10 +1,27 @@
+use js_sys::{JsString, Promise};
+use node_sys::EventEmitter;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
 extern {
-    #[wasm_bindgen]
+    #[wasm_bindgen(extends = EventEmitter)]
     pub type IpcRenderer;
 
     #[wasm_bindgen(js_name = "ipcRenderer")]
     pub static ipc_renderer: IpcRenderer;
+
+    #[wasm_bindgen(method, variadic)]
+    pub fn invoke(this: &IpcRenderer, channel: &JsString, args: Box<[JsValue]>) -> Promise;
+
+    #[wasm_bindgen(method, variadic)]
+    pub fn send(this: &IpcRenderer, channel: &JsString, args: Box<[JsValue]>);
+
+    #[wasm_bindgen(method, variadic, js_name = "sendSync")]
+    pub fn send_sync(this: &IpcRenderer, channel: &JsString, args: Box<[JsValue]>) -> JsValue;
+
+    #[wasm_bindgen(method, variadic, js_name = "sendTo")]
+    pub fn send_to(this: &IpcRenderer, web_contents_id: u32, channel: &JsString, args: Box<[JsValue]>);
+
+    #[wasm_bindgen(method, variadic, js_name = "sendToHost")]
+    pub fn send_to_host(this: &IpcRenderer, channel: &JsString, args: Box<[JsValue]>);
 }
