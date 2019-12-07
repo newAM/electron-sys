d3a4094db03a87aeb0961d1018ce4b3a66166970
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 23:42:03 -0700

    Bindings for screen module

--- src/module/screen.rs
+++ src/module/screen.rs
@@ -1,3 +1,8 @@
+use crate::{
+    class::BrowserWindow,
+    interface::{Display, Point, Rectangle},
+};
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -6,4 +11,30 @@ extern {
     pub type Screen;
 
     pub static screen: Screen;
+
+    // FIXME: event overloads
+
+    #[wasm_bindgen(method, js_name = "dipToScreenPoint")]
+    pub fn dip_to_screen_point(this: &Screen, point: Point) -> Point;
+
+    #[wasm_bindgen(method, js_name = "dipToScreenPoint")]
+    pub fn dip_to_screen_rect(this: &Screen, window: BrowserWindow, rect: Rectangle) -> Rectangle;
+
+    #[wasm_bindgen(method, js_name = "getCursorScreenPoint")]
+    pub fn get_all_displays(this: &Screen) -> Array;
+
+    #[wasm_bindgen(method, js_name = "getDisplayMatching")]
+    pub fn get_display_matching(this: &Screen, rect: Rectangle) -> Display;
+
+    #[wasm_bindgen(method, js_name = "getDisplayNearestPoint")]
+    pub fn get_display_nearest_point(this: &Screen, point: Point) -> Display;
+
+    #[wasm_bindgen(method, js_name = "getPrimaryDisplay")]
+    pub fn get_primary_display(this: &Screen) -> Display;
+
+    #[wasm_bindgen(method, js_name = "screen_to_dip_point")]
+    pub fn screen_to_dip_point(this: &Screen, point: Point) -> Point;
+
+    #[wasm_bindgen(method, js_name = "screen_to_dip_rect")]
+    pub fn screen_to_dip_rect(this: &Screen, rect: Rectangle) -> Rectangle;
 }
