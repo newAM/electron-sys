9ea8617eb1069dd790784620f08f0f246f625b93
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 23:42:11 -0700

    Bindings for session module

--- src/module/session.rs
+++ src/module/session.rs
@@ -1,9 +1,27 @@
+use crate::{class::Session, interface::FromPartitionOptions};
+use js_sys::JsString;
+use node_sys::events::EventEmitter;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
 extern {
-    #[wasm_bindgen]
-    pub type Session;
+    #[wasm_bindgen(extends = EventEmitter)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    /// Docs: http://electronjs.org/docs/api/session
+    pub type SessionModule;
 
-    pub static session: Session;
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "fromPartition")]
+    pub fn from_partition(this: &SessionModule, partition: &JsString, options: Option<FromPartitionOptions>)
+    -> Session;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter, js_name = "defaultSession")]
+    pub fn default_session(this: &SessionModule) -> Session;
 }
