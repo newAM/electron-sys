458ad00b8f25068917545fe8427c05ed6c928dee
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 20:52:39 -0700

    Bindings for powerSaveBlocker module

--- src/module/power_save_blocker.rs
+++ src/module/power_save_blocker.rs
@@ -1,3 +1,4 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,13 @@ extern {
 
     #[wasm_bindgen(js_name = "powerSaveBlocker")]
     pub static power_save_blocker: PowerSaveBlocker;
+
+    #[wasm_bindgen(method, js_name = "isStarted")]
+    pub fn is_started(this: &PowerSaveBlocker, id: u32) -> bool;
+
+    #[wasm_bindgen(method)]
+    pub fn start(this: &PowerSaveBlocker, kind: &JsString) -> u32;
+
+    #[wasm_bindgen(method)]
+    pub fn stop(this: &PowerSaveBlocker, id: u32);
 }
