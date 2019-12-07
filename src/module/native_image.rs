33d040c9991071c6c99d39b4feb776b8c82970be
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 20:28:02 -0700

    Bindings for nativeImage module

--- src/module/native_image.rs
+++ src/module/native_image.rs
@@ -1,9 +1,46 @@
-use js_sys::Object;
+use crate::{
+    class::NativeImage,
+    interface::{CreateFromBitmapOptions, CreateFromBufferOptions},
+};
+use js_sys::{Array, JsString};
+use node_sys::Buffer;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
 extern {
-    #[wasm_bindgen(extends = Object)]
-    /// Docs: http://electronjs.org/docs/api/native-image
-    pub type NativeImage;
+    #[wasm_bindgen]
+    pub type NativeImageModule;
+
+    #[wasm_bindgen(js_name = "nativeImage")]
+    pub static native_image: NativeImageModule;
+
+    #[wasm_bindgen(method, js_name = "createEmpty")]
+    pub fn create_empty(this: &NativeImageModule) -> NativeImage;
+
+    #[wasm_bindgen(method, js_name = "createFromBitmap")]
+    pub fn create_from_bitmap(
+        this: &NativeImageModule,
+        buffer: &Buffer,
+        options: CreateFromBitmapOptions,
+    ) -> NativeImage;
+
+    #[wasm_bindgen(method, js_name = "createFromBuffer")]
+    pub fn create_from_buffer(
+        this: &NativeImageModule,
+        buffer: &Buffer,
+        options: Option<CreateFromBufferOptions>,
+    ) -> NativeImage;
+
+    #[wasm_bindgen(method, js_name = "createFromDataURL")]
+    pub fn create_from_data_url(this: &NativeImageModule, data_url: &JsString) -> NativeImage;
+
+    #[wasm_bindgen(method, js_name = "createFromNamedImage")]
+    pub fn create_from_named_image(
+        this: &NativeImageModule,
+        image_name: &JsString,
+        hsl_shift: Option<Array>,
+    ) -> NativeImage;
+
+    #[wasm_bindgen(method, js_name = "createFromPath")]
+    pub fn create_from_path(this: &NativeImageModule, path: &JsString) -> NativeImage;
 }
