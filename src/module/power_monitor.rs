02d73d610d7a20eb400c18f06c70a81d4da37f66
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 20:49:20 -0700

    Bindings for powerMonitor module

--- src/module/power_monitor.rs
+++ src/module/power_monitor.rs
@@ -1,3 +1,4 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -7,4 +8,10 @@ extern {
 
     #[wasm_bindgen(js_name = "powerMonitor")]
     pub static power_monitor: PowerMonitor;
+
+    #[wasm_bindgen(js_name = "getSystemIdleState")]
+    pub fn get_system_idle_state(this: &PowerMonitor, idle_threshold: u32) -> JsString;
+
+    #[wasm_bindgen(js_name = "getSystemIdleTime")]
+    pub fn get_system_idle_time(this: &PowerMonitor) -> u32;
 }
