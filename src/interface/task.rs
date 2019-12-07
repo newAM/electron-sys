93a1434f2d1f0d39e8ac25fb35153570443be107
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-07 08:58:47 -0700

    Make Task an extern binding; see #20

--- src/interface/task.rs
+++ src/interface/task.rs
@@ -1,19 +1,12 @@
-use js_sys::JsString;
+use js_sys::{JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct Task {
-    arguments: JsString,
-    description: JsString,
-    icon_index: usize,
-    icon_path: JsString,
-    program: JsString,
-    title: JsString,
-    working_directory: Option<JsString>,
-}
+extern {
+    #[wasm_bindgen(extends = Object)]
+    pub type Task;
 
-#[wasm_bindgen]
-impl Task {
+    #[wasm_bindgen(constructor)]
     pub fn new(
         arguments: JsString,
         description: JsString,
@@ -22,85 +15,47 @@ impl Task {
         program: JsString,
         title: JsString,
         working_directory: Option<JsString>,
-    ) -> Task {
-        Task {
-            arguments,
-            description,
-            icon_index,
-            icon_path,
-            program,
-            title,
-            working_directory,
-        }
-    }
+    ) -> Task;
 
-    #[wasm_bindgen(getter)]
-    pub fn arguments(&self) -> JsString {
-        self.arguments.clone()
-    }
+    #[wasm_bindgen(method, getter)]
+    pub fn arguments(this: &Task) -> JsString;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_arguments(&mut self, value: JsString) {
-        self.arguments = value
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_arguments(this: &Task, value: JsString);
 
-    #[wasm_bindgen(getter)]
-    pub fn description(&self) -> JsString {
-        self.description.clone()
-    }
+    #[wasm_bindgen(method, getter)]
+    pub fn description(this: &Task) -> JsString;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_description(&mut self, value: JsString) {
-        self.description = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_description(this: &Task, value: JsString);
 
-    #[wasm_bindgen(getter, js_name = "iconIndex")]
-    pub fn icon_index(&self) -> usize {
-        self.icon_index
-    }
+    #[wasm_bindgen(method, getter, js_name = "iconIndex")]
+    pub fn icon_index(this: &Task) -> usize;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_icon_index(&mut self, value: usize) {
-        self.icon_index = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_icon_index(this: &Task, value: usize);
 
-    #[wasm_bindgen(getter, js_name = "iconPath")]
-    pub fn icon_path(&self) -> JsString {
-        self.icon_path.clone()
-    }
+    #[wasm_bindgen(method, getter, js_name = "iconPath")]
+    pub fn icon_path(this: &Task) -> JsString;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_icon_path(&mut self, value: JsString) {
-        self.icon_path = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_icon_path(this: &Task, value: JsString);
 
-    #[wasm_bindgen(getter)]
-    pub fn program(&self) -> JsString {
-        self.program.clone()
-    }
+    #[wasm_bindgen(method, getter)]
+    pub fn program(this: &Task) -> JsString;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_program(&mut self, value: JsString) {
-        self.program = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_program(this: &Task, value: JsString);
 
-    #[wasm_bindgen(getter)]
-    pub fn title(&self) -> JsString {
-        self.title.clone()
-    }
+    #[wasm_bindgen(method, getter)]
+    pub fn title(this: &Task) -> JsString;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_title(&mut self, value: JsString) {
-        self.title = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_title(this: &Task, value: JsString);
 
-    #[wasm_bindgen(getter, js_name = "workingDirectory")]
-    pub fn working_directory(&self) -> Option<JsString> {
-        self.working_directory.clone()
-    }
+    #[wasm_bindgen(method, getter, js_name = "workingDirectory")]
+    pub fn working_directory(this: &Task) -> Option<JsString>;
 
-    #[wasm_bindgen(setter)]
-    pub fn set_working_directory(&mut self, value: Option<JsString>) {
-        self.working_directory = value;
-    }
+    #[wasm_bindgen(method, setter)]
+    pub fn set_working_directory(this: &Task, value: Option<JsString>);
 }
