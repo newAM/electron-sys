7b9870abff64e62886c56e86d55cba9e9acbc279
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 21:09:27 -0700

    Bindings for protocol module

--- src/module/protocol.rs
+++ src/module/protocol.rs
@@ -1,9 +1,7 @@
+use crate::interface::Protocol;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
 extern {
-    #[wasm_bindgen]
-    pub type Protocol;
-
     pub static protocol: Protocol;
 }
