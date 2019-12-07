da06a4caeb0aeff79fba6f297428b98fea6a4b1e
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-06 22:58:39 -0700

    Bindings for remote module

--- src/module/remote.rs
+++ src/module/remote.rs
@@ -1,3 +1,6 @@
+use crate::class::{BrowserWindow, WebContents};
+use js_sys::JsString;
+use node_sys::Process;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "electron")]
@@ -6,4 +9,27 @@ extern {
     pub type Remote;
 
     pub static remote: Remote;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "getCurrentWebContents")]
+    pub fn get_current_web_contents(this: &Remote) -> WebContents;
+
+    #[wasm_bindgen(method, js_name = "getCurrentWindow")]
+    pub fn get_current_window(this: &Remote) -> BrowserWindow;
+
+    #[wasm_bindgen(method, js_name = "getGlobal")]
+    pub fn get_global(this: &Remote, name: &JsString) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn require(this: &Remote, module: &JsString) -> JsValue;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter)]
+    pub fn process(this: &Remote) -> Process;
 }
