4fa18e9c51affc81cc316e1bf8c95d4fe46126dc
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-20 19:17:18 -0700

    Fix js_names for PowerMonitor methods

--- src/module/power_monitor.rs
+++ src/module/power_monitor.rs
@@ -9,9 +9,9 @@ extern {
     #[wasm_bindgen(js_name = "powerMonitor")]
     pub static power_monitor: PowerMonitor;
 
-    #[wasm_bindgen(js_name = "getSystemIdleState")]
+    #[wasm_bindgen(method, js_name = "getSystemIdleState")]
     pub fn get_system_idle_state(this: &PowerMonitor, idle_threshold: u32) -> JsString;
 
-    #[wasm_bindgen(js_name = "getSystemIdleTime")]
+    #[wasm_bindgen(method, js_name = "getSystemIdleTime")]
     pub fn get_system_idle_time(this: &PowerMonitor) -> u32;
 }
