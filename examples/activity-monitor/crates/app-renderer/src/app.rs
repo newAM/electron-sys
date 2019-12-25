9fb9cd5681e09559d41435a23267d8f90de848fe
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-25 14:09:32 -0700

    Stubs for app logic

--- examples/activity-monitor/crates/app-renderer/src/app.rs
+++ examples/activity-monitor/crates/app-renderer/src/app.rs
@@ -5,6 +5,26 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn set_last_measure_times() {
+    unimplemented!("setLastMeasuretimes")
+}
+
+fn get_datasets() {
+    unimplemented!("getDatasets")
+}
+
+fn update_datasets() {
+    unimplemented!("updateDatasets")
+}
+
+fn get_cpu_times() {
+    unimplemented!("getCpuTimes")
+}
+
+fn draw_chart() {
+    unimplemented!("draw_chart")
+}
+
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
