a51bdf90656ab30d8d9f78defa5ab9cb8df9911a
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-01-11 18:30:02 -0700

    WIP

--- examples/activity-monitor/js/index.js
+++ examples/activity-monitor/js/index.js
@@ -1,1 +1,17 @@
-import("../crates/app-renderer/pkg").catch(console.error);
+/* NOTE: Even though the browser accepts module imports for asynchronous
+ * loading, webpack complains unless dynamic import() is used.
+ */
+
+// import init from "../crates/app-renderer/pkg/activity_monitor_app_renderer.js";
+
+// async function main() {
+//   await init("../crates/app-renderer/pkg/activity_monitor_app_renderer_bg.wasm");
+// }
+
+// main();
+
+import("../crates/app-renderer/pkg/activity_monitor_app_renderer.js")
+    // .then(module => {
+    //     module.init("../crates/app-renderer/pkg/activity_monitor_app_renderer_bg.wasm");
+    // })
+    .catch(console.error);
--- examples/activity-monitor/webpack.config.ts
+++ examples/activity-monitor/webpack.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import webpack from "webpack";
 import CopyPlugin from "copy-webpack-plugin";
-import WasmPackPlugin from "@wasm-tool/wasm-pack-plugin";
 
 const config: webpack.Configuration = {
   mode: "development",
@@ -17,9 +16,6 @@ const config: webpack.Configuration = {
     new CopyPlugin([
       path.resolve(__dirname, "static")
     ]),
-    new WasmPackPlugin({      
-      crateDirectory: path.resolve(__dirname, "crates/app-renderer"),
-    }),
   ],
 };
 
