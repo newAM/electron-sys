e22b60b8b61c4853a8dd0324bd183de32a6cfccc
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-02-19 16:18:28 -0700

    Update hash example

--- examples/hash/crates/app-renderer/src/lib.rs
+++ examples/hash/crates/app-renderer/src/lib.rs
@@ -1,3 +1,4 @@
+use js_sys::JsString;
 use node_sys::crypto;
 use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::HtmlTextAreaElement;
@@ -19,33 +20,37 @@ pub fn run() -> Result<(), JsValue> {
         Closure::wrap(Box::new(move || {
             let text = &text_input.value().into();
 
-            let md5 = crypto::create_hash(&"md5".into(), None)
-                .update_with_encoding(text, Some(&"utf8".into()))
-                .digest_with_encoding(&"hex".into());
+            let md5 = crypto::create_hash("md5", None)
+                .update(text, Some("utf8"))
+                .digest(Some("hex"))
+                .unchecked_into::<JsString>();
             document
                 .get_element_by_id("md5-output")
                 .unwrap_throw()
                 .set_inner_html(String::from(md5).as_str());
 
-            let sha1 = crypto::create_hash(&"sha1".into(), None)
-                .update_with_encoding(text, Some(&"utf8".into()))
-                .digest_with_encoding(&"hex".into());
+            let sha1 = crypto::create_hash("sha1", None)
+                .update(text, Some("utf8"))
+                .digest(Some("hex"))
+                .unchecked_into::<JsString>();
             document
                 .get_element_by_id("sha1-output")
                 .unwrap_throw()
                 .set_inner_html(String::from(sha1).as_str());
 
-            let sha256 = crypto::create_hash(&"sha256".into(), None)
-                .update_with_encoding(text, Some(&"utf8".into()))
-                .digest_with_encoding(&"hex".into());
+            let sha256 = crypto::create_hash("sha256", None)
+                .update(text, Some("utf8"))
+                .digest(Some("hex"))
+                .unchecked_into::<JsString>();
             document
                 .get_element_by_id("sha256-output")
                 .unwrap_throw()
                 .set_inner_html(String::from(sha256).as_str());
 
-            let sha512 = crypto::create_hash(&"sha512".into(), None)
-                .update_with_encoding(text, Some(&"utf8".into()))
-                .digest_with_encoding(&"hex".into());
+            let sha512 = crypto::create_hash("sha512", None)
+                .update(text, Some("utf8"))
+                .digest(Some("hex"))
+                .unchecked_into::<JsString>();
             document
                 .get_element_by_id("sha512-output")
                 .unwrap_throw()
